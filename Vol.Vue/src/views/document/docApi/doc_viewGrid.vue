180b1108fab11194d141219aa7deb1f66d6e070a
jxx <283591387@qq.com.com>
2019-11-19 15:06:12 +0800

    框架文档1.0编写中...

--- Vol.Vue/src/views/document/docApi/doc_viewGrid.vue
+++ Vol.Vue/src/views/document/docApi/doc_viewGrid.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
+          <div class="i-text">
+      <h2>关于ViewGird组件</h2>
+    </div>
     <Alert type="warning">
-      关于ViewGird组件
+      
       <template slot="desc">
         <p>1、ViewGird组件也是前端框架的核心，所有由代码生成的页面的基础功能全部依赖此组件，也可以单独使用此组件。</p>
         <p>2、组件默认包括(单表、主从表)：查询、添加、修改、删除、导入、导出、审核功能</p>
@@ -9,5 +12,13 @@
          <p>4、如果需要一对多或多对多表单等操作，在ViewGrid提供的位置进行扩展组件编写即可。</p>
       </template>
     </Alert>
+    <div class="i-text">
+      <h2>组件结构图</h2>
+    </div>
+        <pre class="bg-ms">
+        <code>
+        还没画好;
+        </code>
+    </pre>
   </div>
 </template>
