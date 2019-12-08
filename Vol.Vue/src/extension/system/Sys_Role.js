e436ae3ec325d6a85276578c402ddc25436bb686
jxx <283591387@qq.com.com>
2019-12-08 22:01:41 +0800

    优化角色页面上级角色选择

--- Vol.Vue/src/extension/system/Sys_Role.js
+++ Vol.Vue/src/extension/system/Sys_Role.js
@@ -3,8 +3,8 @@
 let extension = {
     components: {//动态扩充组件或组件路径
         //表单header、content、footer对应位置扩充的组件
-        gridHeader:'',
-        gridBody:'',
+        gridHeader: '',
+        gridBody: '',
         gridFooter: '',
         //弹出框(修改、编辑、查看)header、content、footer对应位置扩充的组件
         modelHeader: '',
@@ -14,10 +14,22 @@ let extension = {
     buttons: [],//扩展的按钮
     methods: {//事件扩展
         onInit() {
-            this.editFormOptions.forEach(x=>{
-                x.forEach(item=>{
-                    if (item.field=='ParentId') {
-                        item.min=0;
+            this.columns.forEach(x => {
+                if (x.field == 'ParentId') {
+                    x.title = "上级角色";
+                    x.bind = { key: "roles" }
+                }
+            });
+            this.editFormOptions.forEach(x => {
+                x.forEach(item => {
+                    if (item.field == 'ParentId') {
+                        item.min = 0;
+                    }
+                    if (item.field == 'ParentId') {
+                        item.title = "上级角色";
+                        item.type = 'select';
+                        item.dataKey = "roles";
+                        //item.data = [{ key:1,value:'超级管理员' }]
                     }
                 })
             })
