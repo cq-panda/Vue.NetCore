7bdee032624366be16450d5b871a28167ca1bf6c
jxx <283591387@qq.com.com>
2019-12-23 19:04:43 +0800

    添加登陆页面工信部网站备案号

--- Vol.Vue/src/views/Login.vue
+++ Vol.Vue/src/views/Login.vue
@@ -59,6 +59,7 @@
       </a>
       <a @click="()=>{this.$Message.info('还没写好')}">博客园介绍</a>
       <a>QQ群：还没想好</a>
+      <a href="http://www.beian.miit.gov.cn/" target="_blank">京ICP备19056538号-1</a>
     </div>
   </div>
 </template>
@@ -103,6 +104,7 @@ export default {
 
 <style lang="less" scoped>
 .tm-bg {
+  min-width: 700px;
   background-color: #330000;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 800 400'%3E%3Cdefs%3E%3CradialGradient id='a' cx='396' cy='281' r='514' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%233b3b6b'/%3E%3Cstop offset='1' stop-color='%23330000'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='400' y1='148' x2='400' y2='333'%3E%3Cstop offset='0' stop-color='%23146cc9' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23146cc9' stop-opacity='0.5'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='800' height='400'/%3E%3Cg fill-opacity='0.5'%3E%3Ccircle fill='url(%23b)' cx='267.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='532.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='400' cy='30' r='300'/%3E%3C/g%3E%3C/svg%3E");
   background-attachment: fixed;
@@ -224,7 +226,6 @@ input:-webkit-autofill {
 }
 .login-footer {
   padding: 10px;
-  color: white;
   background: #4c4b4b;
   text-align: center;
   font-size: 16px;
@@ -241,7 +242,7 @@ input:-webkit-autofill {
   }
   a {
     margin-left: 30px;
-    color: white;
+    color: #f9ebd0;
   }
 }
 </style>
