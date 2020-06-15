cb8eeb9be98f83a9af06e75cc99656bf4d013ae6
风口浪尖的猪 <283591387@qq.com>
2020-06-15 19:40:51 +0800

    Update App_Appointment.cs

--- Vue.Net/VOL.Entity/DomainModels/Appointment/App_Appointment.cs
+++ Vue.Net/VOL.Entity/DomainModels/Appointment/App_Appointment.cs
@@ -13,7 +13,7 @@ using VOL.Entity.SystemModels;
 
 namespace VOL.Entity.DomainModels
 {
-    [Table("app_appointment")]
+    [Table("App_Appointment")]
     public class App_Appointment:BaseEntity
     {
         /// <summary>
