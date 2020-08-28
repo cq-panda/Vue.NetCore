3616a1088ff97b4a85e531c4935dfd5ef266da32
jxx <283591387>
2020-08-29 00:10:36 +0800

    优化数据源配置自定义sql提示信息

--- 开发版dev/Vue.NetCore/Vol.Vue/src/extension/system/Sys_Dictionary.js
+++ 开发版dev/Vue.NetCore/Vol.Vue/src/extension/system/Sys_Dictionary.js
@@ -1,7 +1,6 @@
-
 import test from "./test.vue";
 let extension = {
-    components: {//动态扩充组件或组件路径
+    components: { //动态扩充组件或组件路径
         //表单header、content、footer对应位置扩充的组件
         //扩展组件引入方式
         ///test
@@ -15,31 +14,40 @@ let extension = {
         modelBody: '',
         modelFooter: ''
     },
-    buttons: [],//扩展的按钮
-    methods: {//事件扩展
+    buttons: [], //扩展的按钮
+    methods: { //事件扩展
         onInit() {
             this.editFormOptions.forEach(x => {
                 x.forEach(item => {
                     if (item.field == 'ParentId') {
                         item.min = 0;
                     }
+                    if (item.field == "DbSql") {
+                        item.placeholder = "如果从数据库加载数据源，请按此格式配置sql语句：select orderType as key,orderName as value from order";
+                    }
                 })
             })
             this.detailOptions.columns.forEach(x => {
-                if (x.field == 'OrderNo') {
-                    x.summary = true;
-                }
-            })
-            //保存后不关闭编辑框
+                    if (x.field == 'OrderNo') {
+                        x.summary = true;
+                    }
+                })
+                //保存后不关闭编辑框
             this.boxOptions.saveClose = false;
         },
         onInited() {
             this.height = this.height - 36;
         },
+        addBefore(formData) {
+            return this.saveBefore(formData);
+        },
+        updateBefore(formData) {
+            return this.saveBefore(formData);
+        },
         saveBefore(formData) {
-            if (this.editFormFileds.DbSql
-                && (this.editFormFileds.DbSql.indexOf('value') == -1
-                    || this.editFormFileds.DbSql.indexOf('key') == -1)
+            if (this.editFormFileds.DbSql &&
+                (this.editFormFileds.DbSql.indexOf('value') == -1 ||
+                    this.editFormFileds.DbSql.indexOf('key') == -1)
             ) {
                 this.$message.error("sql语句必须包括key/value字段,如:select orderType as key,orderName as value from order");
                 return false;
