65768e5827306fe9d2ea764b858569010d866b12
jxx <283591387@qq.com>
2020-12-28 22:59:52 +0800

    优化首页菜单只显示根据节点为0的数据

--- 开发版dev/Vue.NetCore/Vol.Vue/src/components/basic/VolElementMenu.vue
+++ 开发版dev/Vue.NetCore/Vol.Vue/src/components/basic/VolElementMenu.vue
@@ -1,80 +1,111 @@
 <template>
   <div class="vol-el-menu">
-    <el-menu close="vol-el-menu--vertical"
-             :unique-opened="true"
-             @select="select"
-             :collapse="isCollapse"
-             @open="handleOpen"
-             @close="handleClose">
-      <template v-for="(item) in treeList">
-        <el-submenu :key="item.id"
-                    :index="''+item.id"
-                    v-if="item.children.length&&item.enable==1">
+    <el-menu
+      close="vol-el-menu--vertical"
+      :unique-opened="true"
+      @select="select"
+      :collapse="isCollapse"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <template v-for="item in treeList">
+        <el-submenu
+          :key="item.id"
+          :index="'' + item.id"
+          v-if="item.children.length && item.enable == 1"
+        >
           <template slot="title">
-            <Icon :type="item.icon||'ios-aperture'" />
+            <Icon :type="item.icon || 'ios-aperture'" />
             <span slot="title"> {{ item.name }}</span>
           </template>
-          <vol-element-menu-child :list="item.children"></vol-element-menu-child>
-
+          <vol-element-menu-child
+            :list="item.children"
+          ></vol-element-menu-child>
         </el-submenu>
         <template v-else>
-          <el-menu-item class="menu-item-lv1"
-                        v-if="item.enable==1"
-                        :key="item.id"
-                        :index="''+item.id">
-            <Icon :type="item.icon||'ios-aperture'" />
+          <el-menu-item
+            class="menu-item-lv1"
+            v-if="item.enable == 1"
+            :key="item.id"
+            :index="'' + item.id"
+          >
+            <Icon :type="item.icon || 'ios-aperture'" />
             <span slot="title"> {{ item.name }}</span>
           </el-menu-item>
         </template>
       </template>
-
     </el-menu>
-
   </div>
 </template>
 
 <script>
-import VolElementMenuChild from './VolElementMenuChild'
+import VolElementMenuChild from "./VolElementMenuChild";
 export default {
   components: {
-    "vol-element-menu-child": VolElementMenuChild
+    "vol-element-menu-child": VolElementMenuChild,
   },
   props: {
     isCollapse: {
       type: Boolean,
-      default: false
+      default: false,
     },
     onSelect: {
       type: Function,
-      default: x => {
+      default: (x) => {
         console.log(x);
-      }
+      },
     },
     list: {
       type: Array,
-      default: []
-    }
+      default: [],
+    },
+    rootId: {
+      type: Object,
+      default: 0,
+    },
   },
-  data () {
+  data() {
     return {
-      treeList: []
-    }
+      treeList: [],
+    };
   },
-  created () {
-    this.treeList = this.base.convertTree(this.list, () => { })
+  created() {
+    this.treeList = this.convertTree(this.list);
   },
   methods: {
-    select (index, indexPath) {
-      let _item = this.list.find(x => { return x.id == index });
+    convertTree(data) {
+      var root_data = [];
+      data.forEach((x) => {
+        if (!x.children) x.children = [];
+        if (x.parentId === this.rootId) {
+          x.isRoot = true;
+          if (!x.hasOwnProperty("enable")) x.enable = 1;
+          root_data.push(x);
+          this.getTree(x.id, x, data);
+        }
+      });
+      return root_data;
+    },
+    getTree(id, node, data) {
+      data.forEach((x) => {
+        if (!x.hidden && x.parentId == id) {
+          if (!node.children) node.children = [];
+          node.children.push(x);
+          this.getTree(x.id, x, data);
+        }
+      });
+    },
+    select(index, indexPath) {
+      let _item = this.list.find((x) => {
+        return x.id == index;
+      });
       this.onSelect(index, _item);
-      this.$router.push({ path: _item.path })
+      this.$router.push({ path: _item.path });
     },
-    handleOpen () {
-
-    },
-    handleClose () { }
-  }
-}
+    handleOpen() {},
+    handleClose() {},
+  },
+};
 </script>
 <style scoped>
 .vol-el-menu {
