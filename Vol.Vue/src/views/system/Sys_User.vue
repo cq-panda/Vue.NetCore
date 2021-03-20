c99389344d8a8a66c7faff3b6c73be4d27e74daa
jxx1 <283591387@qq.com>
2021-03-20 13:49:53 +0800

    缩进用户管理页面宽度

--- Vol.Vue/src/views/system/Sys_User.vue
+++ Vol.Vue/src/views/system/Sys_User.vue
@@ -6,11 +6,15 @@
  -->
 <template>
     <div>
-        <view-grid :columns="columns"
+        <view-grid
+                   ref="grid"
+                   :columns="columns"
                    :detail="detail"
-                   :editFormFileds="editFormFileds"
+                   :editFormFields="editFormFields"
+                   :editFormFileds="editFormFields"
                    :editFormOptions="editFormOptions"
-                   :searchFormFileds="searchFormFileds"
+                   :searchFormFields="searchFormFields"
+                   :searchFormFileds="searchFormFields"
                    :searchFormOptions="searchFormOptions"
                    :table="table"
                    :extend="extend">
@@ -36,33 +40,33 @@
                     sortName: "User_Id"
                 },
                 extend: extend,
-                editFormFileds: {"UserName":"","UserTrueName":"","Role_Id":"","IsRegregisterPhone":"","Gender":"","Enable":"","CreateDate":"","Creator":"","Remark":"","HeadImageUrl":""},
+                editFormFields: {"UserName":"","UserTrueName":"","Role_Id":"","IsRegregisterPhone":"","Gender":"","Enable":"","CreateDate":"","Creator":"","Remark":"","HeadImageUrl":""},
                 editFormOptions: [[{"title":"帐号","required":true,"field":"UserName","disabled":true},
                                {"title":"真实姓名","required":true,"field":"UserTrueName","type":"text"}],
-                              [{"dataKey":"roles","title":"角色","required":true,"field":"Role_Id","type":"drop"},
-                               {"dataKey":"isphone","title":"手机用户","required":true,"field":"IsRegregisterPhone","type":"drop"}],
-                              [{"dataKey":"gender","title":"性别","field":"Gender","type":"drop"},
-                               {"dataKey":"enable","title":"是否可用","required":true,"field":"Enable","type":"drop"}],
+                              [{"dataKey":"roles","data":[],"title":"角色","required":true,"field":"Role_Id","type":"select"},
+                               {"dataKey":"isphone","data":[],"title":"手机用户","required":true,"field":"IsRegregisterPhone","type":"select"}],
+                              [{"dataKey":"gender","data":[],"title":"性别","field":"Gender","type":"select"},
+                               {"dataKey":"enable","data":[],"title":"是否可用","required":true,"field":"Enable","type":"select"}],
                               [{"title":"注册时间","field":"CreateDate","disabled":true},
                                {"title":"创建人","field":"Creator","disabled":true}],
                               [{"title":"备注","field":"Remark","colSize":12,"type":"textarea"}],
-                              [{"title":"头像","field":"HeadImageUrl","type":"img"}]],
-                searchFormFileds: {"UserName":"","Gender":"","DeptName":"","Role_Id":"","Token":"","AppType":"","UserTrueName":"","CreateDate":"","IsRegregisterPhone":"","PhoneNo":"","Enable":"","LastLoginDate":"","Address":"","Email":""},
-                searchFormOptions: [[{"title":"帐号","field":"UserName"},{"dataKey":"gender","title":"性别","field":"Gender","type":"drop"},{"title":"真实姓名","field":"UserTrueName"}],[{"title":"部门","field":"DeptName"},{"dataKey":"roles","title":"角色","field":"Role_Id","type":"drop"},{"title":"Token","field":"Token"}],[{"dataKey":"ut","title":"登陆设备类型","field":"AppType","type":"dropList"},{"dataKey":"isphone","title":"手机用户","field":"IsRegregisterPhone","type":"drop"},{"title":"手机号","field":"PhoneNo"}],[{"dataKey":"enable","title":"是否可用","field":"Enable","type":"drop"},{"title":"地址","field":"Address"},{"title":"Email","field":"Email"}],[{"title":"注册时间","field":"CreateDate","type":"datetime"},{"title":"最后登陆时间","field":"LastLoginDate","type":"datetime"}]],
-                columns: [{field:'UserName',title:'帐号',type:'string',link:true,width:120,readonly:true,require:true,align:'left',sortable:true},
+                              [{"title":"头像","required":true,"field":"HeadImageUrl","type":"img"}]],
+                searchFormFields: {"UserName":"","Gender":"","DeptName":"","Role_Id":"","Token":"","AppType":[],"UserTrueName":"","CreateDate":"","IsRegregisterPhone":"","PhoneNo":"","Enable":"","LastLoginDate":"","Address":"","Email":""},
+                searchFormOptions: [[{"title":"帐号","field":"UserName"},{"dataKey":"gender","data":[],"title":"性别","field":"Gender","type":"select"},{"title":"真实姓名","field":"UserTrueName"}],[{"title":"部门","field":"DeptName"},{"dataKey":"roles","data":[],"title":"角色","field":"Role_Id","type":"select"},{"title":"Token","field":"Token"}],[{"dataKey":"ut","data":[],"title":"登陆设备类型","field":"AppType","type":"selectList"},{"dataKey":"isphone","data":[],"title":"手机用户","field":"IsRegregisterPhone","type":"select"},{"title":"手机号","field":"PhoneNo"}],[{"dataKey":"enable","data":[],"title":"是否可用","field":"Enable","type":"select"},{"title":"地址","field":"Address"},{"title":"Email","field":"Email"}],[{"title":"注册时间","field":"CreateDate","type":"datetime"},{"title":"最后登陆时间","field":"LastLoginDate","type":"datetime"}]],
+                columns: [{field:'UserName',title:'帐号',type:'string',link:true,width:110,readonly:true,require:true,align:'left',sortable:true},
                        {field:'User_Id',title:'User_Id',type:'int',width:90,hidden:true,readonly:true,require:true,align:'left'},
                        {field:'Gender',title:'性别',type:'int',bind:{ key:'gender',data:[]},width:100,align:'left'},
-                       {field:'HeadImageUrl',title:'头像',type:'img',width:150,align:'left'},
+                       {field:'HeadImageUrl',title:'头像',type:'img',width:100,require:true,align:'left'},
                        {field:'Dept_Id',title:'Dept_Id',type:'int',width:90,hidden:true,align:'left'},
                        {field:'DeptName',title:'部门',type:'string',width:150,hidden:true,align:'left'},
-                       {field:'Role_Id',title:'角色',type:'int',bind:{ key:'roles',data:[]},width:150,require:true,align:'left'},
+                       {field:'Role_Id',title:'角色',type:'int',bind:{ key:'roles',data:[]},width:120,require:true,align:'left'},
                        {field:'RoleName',title:'RoleName',type:'string',width:90,hidden:true,require:true,align:'left'},
                        {field:'Token',title:'Token',type:'string',width:180,hidden:true,align:'left'},
                        {field:'AppType',title:'登陆设备类型',type:'int',bind:{ key:'ut',data:[]},width:150,hidden:true,align:'left'},
-                       {field:'UserTrueName',title:'真实姓名',type:'string',width:150,require:true,align:'left'},
+                       {field:'UserTrueName',title:'真实姓名',type:'string',width:120,require:true,align:'left'},
                        {field:'CreateDate',title:'注册时间',type:'datetime',width:150,readonly:true,align:'left',sortable:true},
                        {field:'IsRegregisterPhone',title:'手机用户',type:'int',bind:{ key:'isphone',data:[]},width:120,hidden:true,require:true,align:'left'},
-                       {field:'PhoneNo',title:'手机号',type:'string',width:150,hidden:true,align:'left'},
+                       {field:'PhoneNo',title:'手机号',type:'string',width:150,hidden:true,require:true,align:'left'},
                        {field:'Tel',title:'Tel',type:'string',width:90,hidden:true,align:'left'},
                        {field:'CreateID',title:'CreateID',type:'int',width:90,hidden:true,align:'left'},
                        {field:'Creator',title:'创建人',type:'string',width:130,readonly:true,align:'left'},
