39f5f31950dc1b1fc48bd269f4691c729963a4dd
jxx1 <283591387@qq.com>
2021-04-18 19:11:29 +0800

    重写volupload文件上传组件

--- Vol.Vue/src/components/basic/VolUpload.vue
+++ Vol.Vue/src/components/basic/VolUpload.vue
@@ -1,72 +1,79 @@
 <template>
   <div class="upload-container">
     <div>
-      <div class="input-btns"
-           style="margin-bottom: 10px;">
-        <input ref="input"
-               type="file"
-               style="display:none;"
-               @change="handleChange"
-               :multiple="multiple" />
-        <div v-if="img"
-             class="upload-img">
+      <div class="input-btns" style="margin-bottom: 10px">
+        <input
+          ref="input"
+          type="file"
+          style="display: none"
+          @change="handleChange"
+          :multiple="multiple"
+        />
+        <div v-if="img" class="upload-img">
           <!-- v-for="(file,index) in fileInfo.length>0?fileInfo: files" -->
-          <div v-for="(file,index) in  files.length>0?files:fileInfo"
-               :key="index"
-               class="img-item">
+          <div v-for="(file, index) in files" :key="index" class="img-item">
             <div class="operation">
               <div class="action">
-                <Icon type="md-eye"
-                      @click="previewImg(index)"
-                      class="view"></Icon>
-                <Icon type="md-close"
-                      @click="removeFile(index)"
-                      class="remove"></Icon>
+                <Icon
+                  type="md-eye"
+                  @click="previewImg(index)"
+                  class="view"
+                ></Icon>
+                <Icon
+                  type="md-close"
+                  @click="removeFile(index)"
+                  class="remove"
+                ></Icon>
               </div>
               <div class="mask"></div>
             </div>
-            <img :src="getImgSrc(file)"
-                 :onerror="errorImg" />
+
+            <img :src="getImgSrc(file, index)" :onerror="errorImg" />
           </div>
-          <div v-show="!autoUpload||(autoUpload&&files.length<maxFile&&fileInfo.length<maxFile)"
-               class="img-selector"
-               :class="getSelector()">
-            <div class="selector"
-                 @click="handleClick">
+          <div
+            v-show="!autoUpload || (autoUpload && files.length < maxFile)"
+            class="img-selector"
+            :class="getSelector()"
+          >
+            <div class="selector" @click="handleClick">
               <Icon type="ios-camera"></Icon>
             </div>
-            <div v-if="!autoUpload"
-                 class="s-btn"
-                 :class="{readonly:changed}"
-                 @click="upload">
-              <div>{{loadText}}</div>
+            <div
+              v-if="!autoUpload"
+              class="s-btn"
+              :class="{ readonly: changed }"
+              @click="upload"
+            >
+              <div>{{ loadText }}</div>
             </div>
           </div>
         </div>
-        <Button v-else
-                @click="handleClick"
-                icon="ios-cloud-upload-outline">选择{{img?'图片':'文件'}}</Button>
+        <Button v-else @click="handleClick" icon="ios-cloud-upload-outline"
+          >选择{{ img ? "图片" : "文件" }}</Button
+        >
 
-        <Button v-if="!autoUpload&&!img"
-                type="info"
-                :disabled="changed"
-                icon="md-arrow-round-up"
-                @click="upload"
-                :loading="loadingStatus">上传文件</Button>
+        <Button
+          v-if="!autoUpload && !img"
+          type="info"
+          :disabled="changed"
+          icon="md-arrow-round-up"
+          @click="upload(true)"
+          :loading="loadingStatus"
+          >上传文件</Button
+        >
       </div>
       <slot></slot>
       <div v-if="desc">
-        <Alert show-icon>{{getText()}}文件大小不超过{{maxSize||3}}M</Alert>
+        <Alert show-icon
+          >{{ getText() }}文件大小不超过{{ maxSize || 3 }}M</Alert
+        >
       </div>
       <slot name="content"></slot>
       <div v-if="!img">
-        <ul class="upload-list"
-            v-show="fileList">
-          <li class="list-file"
-              v-for="(file,index) in  files.length>0?files:fileInfo"
-              :key="index">
+        <ul class="upload-list" v-show="fileList">
+          <li class="list-file" v-for="(file, index) in files" :key="index">
             <a>
-              <span @click="fileOnClick(index,file)">
+              <span @click="fileOnClick(index, file)">
                 <Icon :type="format(file)"></Icon>
                 {{ file.name }}
               </span>
@@ -75,8 +82,7 @@
               style="margin-left:15px;"
               >大小{{(file.size / 1024).toFixed(2)}}KB</span>-->
             </a>
-            <span @click="removeFile(index)"
-                  class="file-remove">
+            <span @click="removeFile(index)" class="file-remove">
               <Icon type="md-close"></Icon>
             </span>
           </li>
@@ -94,70 +100,70 @@ export default {
       //是否显示默认介绍
       //是否多选
       type: Boolean,
-      default: false
+      default: false,
     },
     fileInfo: {
       //用于接收上传的文件，也可以加以默认值，显示已上传的文件，用户上传后会覆盖默认值
       type: Array,
       default: () => {
         return [];
-      } //格式[{name:'1.jpg',path:'127.0.01/1.jpg'}]
+      }, //格式[{name:'1.jpg',path:'127.0.01/1.jpg'}]
     },
     downLoad: {
       //是否可以点击文件下载
       type: Boolean,
-      default: true
+      default: true,
     },
     multiple: {
       //是否多选
       type: Boolean,
-      default: false
+      default: false,
     },
     maxFile: {
       //最多可选文件数量，必须multiple=true，才会生效
       type: Number,
-      default: 5
+      default: 5,
     },
     maxSize: {
       //文件限制大小3M
       type: Number,
-      default: 3
+      default: 3,
     },
 
     autoUpload: {
       //选择文件后是否自动上传
       type: Boolean,
-      default: true
+      default: true,
     },
     img: {
       //图片类型  img>excel>fileTypes三种文件类型优先级
       type: Boolean,
-      default: false
+      default: false,
     },
     excel: {
       //excel文件
       type: Boolean,
-      default: false
+      default: false,
     },
     fileTypes: {
       //指定上传文件的类型
       type: Array,
       default: () => {
         return [];
-      }
+      },
     },
     url: {
       //上传的url
       type: String,
-      default: ""
+      default: "",
     },
     uploadBefore: {
       //返回false会中止执行
       //上传前
       type: Function,
-      default: files => {
+      default: (files) => {
         return true;
-      }
+      },
     },
     uploadAfter: {
       //返回false会中止执行
@@ -165,14 +171,14 @@ export default {
       type: Function,
       default: (result, files) => {
         return true;
-      }
+      },
     },
     onChange: {
       //选择文件时  //返回false会中止执行
       type: Function,
-      default: files => {
+      default: (files) => {
         return true;
-      }
+      },
     },
     // clear: {
     //   //上传完成后是否清空文件列表
@@ -182,29 +188,34 @@ export default {
     fileList: {
       //是否显示选择的文件列表
       type: Boolean,
-      default: true
+      default: true,
     },
     fileClick: {
       //点击文件事件
       type: Function,
       default: (index, file, files) => {
         return true;
-      }
+      },
     },
     removeBefore: {
       //移除文件事件
       type: Function,
       default: (index, file, files) => {
         return true;
-      }
+      },
     },
     append: {
-      //多选时，重新选择文件是否追加(默认重选直接用清原数据),逻辑待处理
+      //此属性已废弃，多文件上传，默认追加文件
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
+    compress: {
+      //开启图片压缩,后面根据需要再完善
+      type: Boolean,
+      default: false,
+    },
   },
-  data () {
+  data() {
     return {
       errorImg: 'this.src="' + require("@/assets/imgs/error-img.png") + '"',
       changed: false, //手动上传成功后禁止重复上传，必须重新选择
@@ -212,34 +223,46 @@ export default {
       files: [],
       bigImg: "",
       loadingStatus: false,
-      loadText: '上传文件'
+      loadText: "上传文件",
     };
   },
-  created () {
+  created() {
     //默认有图片的禁止上传操作
     if (this.fileInfo) {
       this.changed = true;
     }
+    this.cloneFile(this.fileInfo);
+  },
+  watch: {
+    fileInfo: {
+      handler(files) {
+        this.cloneFile(files);
+      },
+      deep: true,
+    },
   },
   methods: {
-    previewImg (index) {
+    cloneFile(files) {
+      this.files = files.map((x) => {
+        return {
+          name: x.name || "未定义文件名",
+          path: x.path,
+        };
+      });
+    },
+    previewImg(index) {
       //查看大图预览模式待完
-      this.base.previewImg(
-        this.getImgSrc(
-          (this.files.length > 0 ? this.files : this.fileInfo)[index]
-        )
-      );
+      this.base.previewImg(this.getImgSrc(this.files[index]));
       //  window.open(this.getImgSrc((this.files.length>0?this.files:this.fileInfo)[index]));
     },
-    getSelector () {
+    getSelector() {
       if (this.autoUpload) {
         return "auto-selector";
       }
       return "submit-selector";
     },
-    getImgSrc (file) {
+    getImgSrc(file, index) {
       if (file.hasOwnProperty("path")) {
-
         if (this.base.isUrl(file.path)) {
           return file.path;
         }
@@ -254,7 +277,7 @@ export default {
       }
       return window.URL.createObjectURL(file);
     },
-    fileOnClick (index, file) {
+    fileOnClick(index, file) {
       if (!this.fileClick(index, file, this.files)) {
         return;
       }
@@ -270,61 +293,70 @@ export default {
         file.path,
         file.name,
         {
-          Authorization: this.$store.getters.getToken()
+          Authorization: this.$store.getters.getToken(),
         },
         this.http.ipAddress
       );
     },
-    getText () {
+    getText() {
       if (this.img) {
         return "只能上传图片,";
       } else if (this.excel) {
         return "只能上传excel文件,";
       }
     },
-    handleClick () {
+    handleClick() {
       this.$refs.input.click();
     },
-    handleChange (e) {
-      this.clearFiles();
+    handleChange(e) {
+      //this.compress开启图片压缩,后面根据需要再完善
+      // this.clearFiles();
       var result = this.checkFile(e.target.files);
       if (!result) {
         return;
       }
+
       this.changed = false;
       //如果传入了FileInfo需要自行处理移除FileInfo
       if (!this.onChange(e.target.files)) {
         return;
       }
+      for (let index = 0; index < e.target.files.length; index++) {
+        const element = e.target.files[index];
+        element.input = true;
+      }
+      if (!this.multiple) {
+        this.files.splice(0);
+      }
       this.files.push(...e.target.files);
-      this.$refs.input.value = null;
 
+      this.$refs.input.value = null;
       if (this.autoUpload && result) {
-        this.upload();
+        this.upload(false);
       }
     },
-    removeFile (index) {
+    removeFile(index) {
       //如果传入了FileInfo需要自行处理移除FileInfo
       //t移除文件
-      let removeFile =
-        this.fileInfo.length > 0 ? this.fileInfo[index] : this.files[index];
-      if (this.fileInfo.length) {
-        this.fileInfo.splice(index, 1);
-      } else {
+      let removeFile = this.files[index];
+      //删除的还没上传的文件
+      if (removeFile.input) {
         this.files.splice(index, 1);
+      } else {
+        this.fileInfo.splice(index, 1);
       }
       if (!this.removeBefore(index, removeFile, this.fileInfo)) {
         return;
       }
     },
-    clearFiles () {
+    clearFiles() {
       this.files.splice(0);
     },
-    getFiles () {
+    getFiles() {
       return this.files;
     },
-    upload () {
-      if (!this.checkFile()) return false;
+    upload(vail) {
+      if (vail && !this.checkFile()) return false;
       if (!this.url) {
         return this.$Message.error({ duration: 5, content: "没有配置好Url" });
       }
@@ -336,7 +368,9 @@ export default {
       }
       var forms = new FormData();
       this.files.forEach(function (file) {
-        forms.append("fileInput", file, file.name);
+        if (file.input) {
+          forms.append("fileInput", file, file.name);
+        }
       });
       // forms.append("fileInput", this.files);
       this.loadingStatus = true;
@@ -344,7 +378,7 @@ export default {
       this.http
         .post(this.url, forms, this.autoUpload ? "正在上传文件" : "")
         .then(
-          x => {
+          (x) => {
             // this.$refs.uploadFile.clearFiles();
             this.loadingStatus = false;
             this.loadText = "上传文件";
@@ -360,28 +394,30 @@ export default {
               // this.files = null;
               return;
             }
-            if (!this.append) {
-              this.fileInfo.splice(0);
-            }
-            this.files.forEach(file => {
-              this.fileInfo.push({ name: file.name, path: x.data + file.name });
-            });
+            //单选清除以前的数据
+            //  if (!this.multiple) {
+            this.fileInfo.splice(0);
+            // }
+            this.fileInfo.push(
+              ...this.files.map((file) => {
+                return { name: file.name, path: x.data + file.name };
+              })
+            );
+            // this.files.forEach((file) => {
+            //   this.fileInfo.push({ name: file.name, path: x.data + file.name });
+            // });
             // if (this.clear) {
-            this.clearFiles();
+            //  this.clearFiles();
             // }
           },
-          error => {
+          (error) => {
             this.loadText = "上传文件";
             this.loadingStatus = false;
           }
         );
     },
-    format (file, checkFileType) {
-      const format =
-        file.name
-          .split(".")
-          .pop()
-          .toLocaleLowerCase() || "";
+    format(file, checkFileType) {
+      const format = file.name.split(".").pop().toLocaleLowerCase() || "";
       let fileIcon = "ios-document-outline";
       if (this.fileTypes.length > 0 && checkFileType != undefined) {
         if (this.fileTypes.indexOf(format) != -1) {
@@ -449,14 +485,13 @@ export default {
 
       return fileIcon;
     },
-    beforeUpload () { },
-    checkFile (files) {
-      if (!files) {
-        files = this.files;
-      }
+    beforeUpload() {},
+    checkFile(inputFiles) {
+      const files = this.files;
+
       if (
         this.multiple &&
-        files.length + this.fileInfo.length > (this.maxFile || 5)
+        files.length + (inputFiles || []).length > (this.maxFile || 5)
       ) {
         this.$Message.error({
           duration: 5,
@@ -465,13 +500,18 @@ export default {
             (this.maxFile || 5) +
             "】" +
             (this.img ? "张图片" : "个文件") +
-            ""
+            "",
         });
         return false;
       }
+      if (!inputFiles) {
+        inputFiles = this.files.filter((x) => {
+          return x.input;
+        });
+      }
       let names = [];
-      for (let index = 0; index < files.length; index++) {
-        const file = files[index];
+      for (let index = 0; index < inputFiles.length; index++) {
+        const file = inputFiles[index];
         if (names.indexOf(file.name) != -1) {
           file.name = "(" + index + ")" + file.name;
         }
@@ -479,14 +519,14 @@ export default {
         if (this.img && !this.format(file, "img")) {
           this.$Message.error({
             duration: 5,
-            content: "选择的文件【" + file.name + "】只能是图片格式"
+            content: "选择的文件【" + file.name + "】只能是图片格式",
           });
           return false;
         }
         if (this.excel && !this.format(file, "excel")) {
           this.$Message.error({
             duration: 5,
-            content: "选择的文件【" + file.name + "】只能是excel文件"
+            content: "选择的文件【" + file.name + "】只能是excel文件",
           });
           return false;
         }
@@ -502,7 +542,7 @@ export default {
               file.name +
               "】只能是【" +
               this.fileTypes.join(",") +
-              "】格式"
+              "】格式",
           });
           return false;
         }
@@ -514,14 +554,14 @@ export default {
               file.name +
               "】不能超过:" +
               (this.maxSize || 3) +
-              "M"
+              "M",
           });
           return false;
         }
       }
       return true;
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
