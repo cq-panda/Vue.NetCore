41f85b3ea87c597b4095a0ba96056bc962d359a5
jxx1 <283591387@qq.com>
2021-07-04 23:00:32 +0800

    增加删除、修改、新增时可以设置Code="-1"强制返回执行结果(具体见【后台基础代码扩展实现】文档)

--- Vue.Net/VOL.Order/Services/Sell/Partial/SellOrderService.cs
+++ Vue.Net/VOL.Order/Services/Sell/Partial/SellOrderService.cs
@@ -2,15 +2,19 @@
  *所有关于SellOrder类的业务代码应在此处编写
 */
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using VOL.Core.BaseProvider;
 using VOL.Core.Enums;
+using VOL.Core.Extensions;
 using VOL.Core.ManageUser;
 using VOL.Core.UserManager;
 using VOL.Core.Utilities;
 using VOL.Entity.DomainModels;
+using VOL.Order.IRepositories;
+using VOL.Order.Repositories;
 
 namespace VOL.Order.Services
 {
@@ -23,70 +27,34 @@ namespace VOL.Order.Services
         //此SellOrderService.cs类由代码生成器生成，默认是没有任何代码，如果需要写业务代码，请在此类中实现
         //如果默认的增、删、改、查、导入、导出、审核满足不了业务，请参考下面的方法进行业务代码扩展(扩展代码是对ServiceFunFilter.cs的实现)
 
-        //==============常用操作=======================
-        //访问数据库
-        //1、使用 repository. 使用原生EF  repository.DbContext
-        //    repository.DbContext.Set<Sys_User>().Find() 
-        //    SellOrderRepository.Instance.Find()        
-        //    SellOrderListRepository.Instance.Find()
-        //    DBServerProvider.SqlDapper
-        //    DBServerProvider.DbContext
-        //    以上方式都能访问数据库
-
-        //2、使用EF事务 repository.DbContextBeginTransaction或 SellOrderRepository.Instance.DbContextBeginTransaction
-
-        //3、Dapper 使用：repository.DapperContext , DBServerProvider.SqlDapper
-
-        //4、获取Memory/Redis对象
-        // base.CacheContext 
-        // AutofacContainerModule.GetService<ICacheService>()
-        // VOL.Core.Utilities.HttpContext.Current.RequestServices
-        // VOL.Core.Utilities.HttpContext.Current.RequestServices.GetService(typeof(ICacheService))
-
-        //5、使用HttpContext：VOL.Core.Utilities.HttpContext
-
-        //获取用户信息/权限 
-        //6、UserContext.Current/UserContext.Current.Permissions
-
-        //7、获取配置信息appsettings.json
-        // AppSetting   /    AppSetting.Configuration
-
-        //8、查看权限验证规则 Vol.Core->Filters文件夹下权限控制类
-
-        //9、其他封装了大量的常用扩展方法Vol.Core->Extensions文件夹下(如：字符串扩展、表达式扩展、实体验证(EntityProperties)扩展方法)
-
-        //10、实体数据合法性校验(ServiceBase.cs有大量实体校验示例)
-        //SellOrder order = new SellOrder();
-        //校验指定字段x.TranNo, x.Auditor的类型、长度等合法性
-        //order.ValidationEntity(x => new { x.TranNo, x.Auditor});
-
-        //11、获取实体的配置信息，如：字段中文名称、最大长度、字段主键等信息
-        // Type type = typeof(SellOrder);
-        //获取主键字段
-        // PropertyInfo key = type.GetKeyProperty();
-        //获取主键字段名
-        // type.GetKeyName();
-        //判断某个字段是否有EditableAttribute属性
-        //  key.ContainsCustomAttributes(typeof(EditableAttribute));
-
-        //12、常用工具类Vol.Core->Utilities 
-
-        //写入日志 : Logger.Info();
-
-        //其中有一部分真实扩展代码实现:Partial->Sys_UserService.cs  , Partial->Sys_RoleService ,Partial->Sys_DictionaryService
-
-        protected override void Init(IRepository<SellOrder> repository)
+        WebResponseContent webResponse = new WebResponseContent();
+        private IHttpContextAccessor _httpContextAccessor;
+        private ISellOrderRepository _repository;
+        [ActivatorUtilitiesConstructor]
+        public SellOrderService(IHttpContextAccessor httpContextAccessor, ISellOrderRepository repository)
+            : base(repository)
         {
+            _httpContextAccessor = httpContextAccessor;
+            _repository = repository;
+            base.Init(_repository);
             //2020.08.15
             //开启多租户功能,开启后会对查询、导出、删除、编辑功能同时生效
             //如果只需要对某个功能生效，如编辑，则在重写编辑方法中设置 IsMultiTenancy = true;
             IsMultiTenancy = true;
         }
-
-
         //查询
         public override PageGridData<SellOrder> GetPageData(PageDataOptions options)
         {
+            //options.Value可以从前台查询的方法提交一些其他参数放到value里面
+            //前端提交方式，见文档：组件api->viewgrid组件里面的searchBefore方法
+            object extraValue = options.Value;
+
+            //此处是从前台提交的原生的查询条件，这里可以自己过滤
+            QueryRelativeList = (List<SearchParameters> parameters) =>
+            {
+
+            };
+
             //2020.08.15
             //设置原生查询的sql语句，这里必须返回select * 表所有字段
             //（先内部过滤数据,内部调用EF方法FromSqlRaw,自己写的sql注意sql注入的问题），不会影响界面上提交的查询
@@ -104,21 +72,24 @@ namespace VOL.Order.Services
             QueryRelativeExpression = (IQueryable<SellOrder> queryable) =>
             {
                 //当前用户只能操作自己(与下级角色)创建的数据,如:查询、删除、修改等操作
-                IQueryable<int> userQuery = RoleContext.GetCurrentAllChildUser();
-                queryable = queryable.Where(x => x.CreateID == UserContext.Current.UserId || userQuery.Contains(x.CreateID ?? 0));
+                //IQueryable<int> userQuery = RoleContext.GetCurrentAllChildUser();
+                //queryable = queryable.Where(x => x.CreateID == UserContext.Current.UserId || userQuery.Contains(x.CreateID ?? 0));
                 return queryable;
             };
 
-            //此处是从前台提交的原生的查询条件，这里可以自己过滤
-            QueryRelativeList = (List<SearchParameters> parameters) =>
-            {
-
-            };
             //指定多个字段进行排序
             OrderByExpression = x => new Dictionary<object, QueryOrderBy>() {
                 { x.CreateDate,QueryOrderBy.Desc },
-                { x.SellNo,QueryOrderBy.Desc}
+                { x.SellNo,QueryOrderBy.Asc}
             };
+
+            //int a = 1;
+            ////指定多个字段按条件进行排序（需要2021.07.04更新LambdaExtensions类后才能使用）
+            //OrderByExpression = x => new Dictionary<object, QueryOrderBy>() {
+            //    { x.CreateDate,QueryOrderBy.Desc },
+            //    { x.SellNo,a==1?QueryOrderBy.Desc:QueryOrderBy.Asc}
+            //};
+
 
             //查询完成后，在返回页面前可对查询的数据进行操作
             GetPageDataOnExecuted = (PageGridData<SellOrder> grid) =>
@@ -156,12 +127,37 @@ namespace VOL.Order.Services
         }
 
         /// <summary>
-        /// 查询业务代码编写
+        /// 查询业务代码编写(从表(明细表查询))
         /// </summary>
         /// <param name="pageData"></param>
         /// <returns></returns>
         public override object GetDetailPage(PageDataOptions pageData)
         {
+            //自定义查询胆细表
+
+            ////明细表自定义查询方式一：EF
+            //var query = SellOrderListRepository.Instance.IQueryablePage<SellOrderList>(
+            //     pageData.Page,
+            //     pageData.Rows,
+            //     out int count,
+            //     x => x.Order_Id == pageData.Value.GetGuid(),
+            //      orderBy: x => new Dictionary<object, QueryOrderBy>() { { x.CreateDate, QueryOrderBy.Desc } }
+            //    );
+            //PageGridData<SellOrderList> detailGrid = new PageGridData<SellOrderList>();
+            //detailGrid.rows = query.ToList();
+            //detailGrid.total = count;
+
+            ////明细表自定义查询方式二：dapper
+            //string sql = "select count(1) from SellOrderList where Order_Id=@orderId";
+            //detailGrid.total = repository.DapperContext.ExecuteScalar(sql, new { orderId = pageData.Value }).GetInt();
+
+            //sql = @$"select * from (
+            //              select *,ROW_NUMBER()over(order by createdate desc) as rowId 
+            //           from SellOrderList where Order_Id=@orderId
+            //        ) as s where s.rowId between {((pageData.Page - 1) * pageData.Rows + 1)} and {pageData.Page * pageData.Rows} ";
+            //detailGrid.rows = repository.DapperContext.QueryList<SellOrderList>(sql, new { orderId = pageData.Value });
+
+            //return detailGrid;
 
             return base.GetDetailPage(pageData);
         }
@@ -173,25 +169,29 @@ namespace VOL.Order.Services
         /// <returns></returns>
         public override WebResponseContent Add(SaveModel saveDataModel)
         {
-            WebResponseContent responseContent = WebResponseContent.Instance;
             //此处saveModel是从前台提交的原生数据，可对数据进修改过滤
             AddOnExecute = (SaveModel saveModel) =>
             {
                 //如果返回false,后面代码不会再执行
-                return responseContent.OK();
+                return webResponse.OK();
             };
             // 在保存数据库前的操作，所有数据都验证通过了，这一步执行完就执行数据库保存
             AddOnExecuting = (SellOrder order, object list) =>
             {
+                //如果设置code=-1会强制返回，不再继续后面的操作,2021.07.04更新LambdaExtensions文件后才可以使用此属性
+                //webResponse.Code = "-1";
+                // webResponse.Message = "测试强制返回";
+                //return webResponse.OK();
+
                 List<SellOrderList> orderLists = list as List<SellOrderList>;
                 if (orderLists == null || orderLists.Count == 0)
                 {//如果没有界面上没有填写明细，则中断执行
-                    return responseContent.Error("必须填写明细数据");
+                    return webResponse.Error("必须填写明细数据");
                 }
                 if (orderLists.Exists(x => x.Qty <= 20))
-                    return responseContent.Error("明细数量必须大于20");
+                    return webResponse.Error("明细数量必须大于20");
 
-                return responseContent.OK();
+                return webResponse.OK();
             };
 
             //此方法中已开启了事务，如果在此方法中做其他数据库操作，请不要再开启事务
@@ -200,9 +200,9 @@ namespace VOL.Order.Services
             {
                 if (order.Qty < 10)
                 {  //如果输入的销售数量<10，会回滚数据库
-                    return responseContent.Error("销售数量必须大于1000");
+                    return webResponse.Error("销售数量必须大于1000");
                 }
-                return responseContent.OK("已新建成功,台AddOnExecuted方法返回的消息");
+                return webResponse.OK("已新建成功,台AddOnExecuted方法返回的消息");
             };
 
             return base.Add(saveDataModel);
@@ -217,34 +217,44 @@ namespace VOL.Order.Services
             //此处saveModel是从前台提交的原生数据，可对数据进修改过滤
             UpdateOnExecute = (SaveModel model) =>
             {
+                ////这里的设置配合下面order.Remark = "888"代码位置使用
+                // saveModel.MainData.TryAdd("Remark", "1231");
+
+                //如果不想前端提交某些可以编辑的字段的值,直接移除字段
+                // saveModel.MainData.Remove("字段");
+
                 //如果返回false,后面代码不会再执行
-                return new WebResponseContent().OK();
+                return webResponse.OK();
+
             };
+
+
+
             //编辑方法保存数据库前处理
             UpdateOnExecuting = (SellOrder order, object addList, object updateList, List<object> delKeys) =>
               {
                   if (order.TranNo == "2019000001810001")
                   {
-                      return new WebResponseContent().Error("不能更新此[" + order.TranNo + "]单号");
+                      //如果设置code=-1会强制返回，不再继续后面的操作,2021.07.04更新LambdaExtensions文件后才可以使用此属性
+                      //webResponse.Code = "-1";
+                      // webResponse.Message = "测试强制返回";
+                      //return webResponse.OK();
+                      return webResponse.Error("不能更新此[" + order.TranNo + "]单号");
                   }
-                  //给Remark在后台设置值: 
-                  /*
-                     给Remark设置值需要注意，
-                     1、代码生成页面必须给此字段设置了编辑行，否则这里设置了值也会被过滤
-                     2、如果不想在编辑页面上显示，给此字段的编辑行设置为0，现生成下model即可
-                     3、编辑行为0时，又需要在后台设置值的，请在设置值前添加到字典里面,如： saveModel.MainData.TryAdd("Remark","")
-                   */
-                  //如果Remark字段编辑行设置的是0，请先给字典设置一个默认空值saveModel.MainData.TryAdd("Remark","")
-                  //给model设置值
-                  // order.Remark = "test";
 
-                  //新增的明细
+                  ////如果要手动设置某些字段的值,值不是前端提交的（代码生成器里面编辑行必须设置为0并生成model）,如Remark字段:
+                  ////注意必须设置上面saveModel.MainData.TryAdd("Remark", "1231")
+                  //order.Remark = "888";
+
+
+                  //新增的明细表
                   List<SellOrderList> add = addList as List<SellOrderList>;
-                  //修改的明细
+                  //修改的明细表
                   List<SellOrderList> update = updateList as List<SellOrderList>;
-                  //删除的行的Id
+                  //删除明细表Id
                   var guids = delKeys?.Select(x => (Guid)x);
-                  return new WebResponseContent().OK();
+
+                  return webResponse.OK();
               };
 
             //编辑方法保存数据库后处理
@@ -258,7 +268,7 @@ namespace VOL.Order.Services
                   List<SellOrderList> update = updateList as List<SellOrderList>;
                   //删除的行的主键
                   var guids = delKeys?.Select(x => (Guid)x);
-                  return new WebResponseContent().OK();
+                  return webResponse.OK();
               };
 
             return base.Update(saveModel);
@@ -276,13 +286,13 @@ namespace VOL.Order.Services
             //删除的行的主键
             DelOnExecuting = (object[] _keys) =>
             {
-                return new WebResponseContent(true);
+                return webResponse.OK();
             };
             //删除后处理
             //删除的行的主键
             DelOnExecuted = (object[] _keys) =>
              {
-                 return new WebResponseContent(true);
+                 return webResponse.OK();
              };
             return base.Del(keys, delList);
         }
@@ -291,12 +301,12 @@ namespace VOL.Order.Services
             //审核保存前处理
             AuditOnExecuting = (List<SellOrder> order) =>
             {
-                return new WebResponseContent().OK();
+                return webResponse.OK();
             };
 
             AuditOnExecuted = (List<SellOrder> order) =>
             {
-                return new WebResponseContent().OK();
+                return webResponse.OK();
             };
             return base.Audit(keys, auditStatus, auditReason);
         }
@@ -317,7 +327,7 @@ namespace VOL.Order.Services
             //list导出的实体，ignore过滤不导出的字段
             ExportOnExecuting = (List<SellOrder> list, List<string> ignore) =>
             {
-                return new WebResponseContent().OK();
+                return webResponse.OK();
             };
 
             return base.Export(pageData);
@@ -330,7 +340,7 @@ namespace VOL.Order.Services
         public override WebResponseContent DownLoadTemplate()
         {
             //指定导出模板的字段,如果不设置DownLoadTemplateColumns，默认导出查所有页面上能看到的列(2020.05.07)
-            DownLoadTemplateColumns =  x => new { x.SellNo, x.TranNo,x.Remark, x.CreateDate };
+            DownLoadTemplateColumns = x => new { x.SellNo, x.TranNo, x.Remark, x.CreateDate };
             return base.DownLoadTemplate();
         }
 
@@ -349,12 +359,13 @@ namespace VOL.Order.Services
             //导入保存前处理(可以对list设置新的值)
             ImportOnExecuting = (List<SellOrder> list) =>
             {
-                return new WebResponseContent(true);
+                return webResponse.OK();
             };
-            //导入后处理
+
+            //导入后处理(已经写入到数据库了)
             ImportOnExecuted = (List<SellOrder> list) =>
             {
-                return new WebResponseContent().OK();
+                return webResponse.OK();
             };
             return base.Import(files);
         }
