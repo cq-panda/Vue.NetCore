0897f549f60209e65221cc08e4835378ba8615aa
jxx1 <283591387@qq.com>
2021-09-05 23:53:21 +0800

    简化编辑器内容发布代码

--- Vol.Vue/src/extension/appmanager/App_News.js
+++ Vol.Vue/src/extension/appmanager/App_News.js
@@ -1,11 +1,4 @@
-//只处只是对当前代码生成后的App_News.vue页面进行扩展文本编辑器
-//动态扩展设置【设置封面】【生成静态页面】【预览页面】按钮
-//因为使用的文本编辑器，需要将弹出框的大小，否则弹出框太小不易操作
-//Content字段为App_News表的文本内容存储字段，在代码生成器中将Content这段的编辑行设置为0可以编辑
-//实现保存或新建前的方法，将文本编辑内容Content提交到后台
-/////////////////////////////////////////////////////
-//所有扩展与属性文档参照:api组件->代码生成后页面viewGrid
-import AsyncLoading from "@/components/basic/AsyncLoading.vue";
+
 let extension = {
     components: {//动态扩充组件或组件路径
         //表单header、content、footer对应位置扩充的组件
@@ -22,48 +15,14 @@ let extension = {
         gridFooter: '',
         //弹出框(修改、编辑、查看)header、content、footer对应位置扩充的组件
         modelHeader: '',
-        modelBody: () => ({ component: import("./App_News/App_NewsModelBody.vue"), loading: AsyncLoading }),
+        modelBody: '',
         modelFooter: ''
     },
     text: "静态文件没有提交到github，本地重新生成下静态页面与设置封面即可预览",
     //如果不需要权限判断直接将扩展按钮加到此处
     //需要权限判断的可按下面initButton方法进行扩展按钮
     buttons: {
-        view: [//ViewGrid查询界面扩展的按钮
-            // {
-            //     name: "设置封面",
-            //     icon: 'md-create',
-            //     value: 'Edit',
-            //     class: '',
-            //     type: 'info',
-            //     index: 2,//显示的位置
-            //     onClick: function () {
-            //         this.setCover();
-            //     }
-            // }],
-            // box: [ //弹出框扩展的按钮
-            //     {
-            //         name: "生成静态页面",
-            //         icon: 'ios-cart',
-            //         type: 'error',
-            //         index: 0,//显示的位置
-            //         onClick: function () { //扩展按钮执行事件
-            //             this.publish();
-            //         }
-            //     }, {
-            //         name: "预览页面",
-            //         icon: 'ios-globe',
-            //         type: 'info',
-            //         index: 1,//显示的位置
-            //         onClick: function () { //扩展按钮执行事件
-            //             if (!this.currentRow
-            //                 || !this.currentRow.Content
-            //                 || !this.currentRow.DetailUrl) {
-            //                 return this.$Message.error("请先【保存】,再点击【生成静态页面】")
-            //             }
-            //             this.preview(this.currentRow);
-            //         }
-            //     }
+        view: [//ViewGrid查询界面扩展的按钮 
         ]
     },
     methods: {
@@ -99,14 +58,13 @@ let extension = {
             this.initButton();
             //将当前vue对象写入vuex来操作kindeditor
             //或者使用 this.$emit("parentCall", $vue => {})在子组件中操作父组件
-            this.$store.getters.data().editor = this;
             //设置保存成功后，不关闭弹出框
             this.boxOptions.saveClose = false;
             //设置查询表格只能单选
             this.single = true;
             //手动设置弹出框高与宽度
-            this.boxOptions.height = document.documentElement.clientHeight * 0.8
-            this.boxOptions.width = document.documentElement.clientWidth * 0.8;
+            // this.boxOptions.height = document.documentElement.clientHeight * 0.8
+            // this.boxOptions.width = document.documentElement.clientWidth * 0.8;
             //设置table表格DetailUrl字段点击预览静态页面
             this.editFormOptions.forEach(x => {
                 x.forEach(item => {
@@ -124,37 +82,25 @@ let extension = {
             })
         },
         modelOpenAfter(row) { }, //点击编辑/新建按钮弹出框后，可以在此处写逻辑，如，从后台获取数据
-        addBefore(formData) {//新建前验证
-            return this.validContent(formData);
-        },
-        updateBefore(formData) { //修改前验证
-            return this.validContent(formData);
-        },
-        validContent(formData) {
-            if (!this.currentRow.Content) {
-                this.$Message.error("请编辑要发布的内容");
-                return false;
-            }
-            formData.mainData.Content = this.currentRow.Content;
-            return true;
-        },
         preview(row) { //预览html页面
+
             if (!row.DetailUrl || row.DetailUrl.indexOf('.html') == -1 || !this.base.isUrl(this.http.ipAddress + row.DetailUrl)) {
                 return this.$Message.error("请先发布静态页面")
             }
-            window.open(this.http.ipAddress + row.DetailUrl);
+            let n = Math.random();
+            window.open(this.http.ipAddress + row.DetailUrl + "?r=" + n);
         },
         publish() { //生成静态页面
             if (!this.currentRow || !this.currentRow[this.table.key]) {
                 return this.$Message.error("请先保存数据")
             }
-            if (!this.currentRow.Content) {
+            if (!this.editFormFields.Content) {
                 return this.$Message.error("请编辑要发布的内容")
             }
-            this.http.post("api/news/createPage", this.currentRow).then(x => {
+            this.http.post("api/news/createPage", this.editFormFields).then(x => {
                 if (x.status) {
-                    // this.editFormFileds.DetailUrl = x.data.url;
-                    this.currentRow.DetailUrl = x.data.url;
+                    this.editFormFileds.DetailUrl = x.data.url;
+                    //  this.currentRow.Content = x.data.url;
                 }
                 this.refresh();
                 return this.$Message.info(x.message)
