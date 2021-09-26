16273f85e5bfaa6cae9d8d0d3fb41754ad823d01
jxx1 <283591387@qq.com>
2021-09-26 23:02:34 +0800

    重写vue2版本编辑弹出框从table选择数据示例

--- Vol.Vue/src/extension/appmanager/App_Expert.js
+++ Vol.Vue/src/extension/appmanager/App_Expert.js
@@ -1,4 +1,5 @@
-//如果嫌render双向绑定麻烦或内容太多，可以使用下面的方式
+
+//通过render动态渲染表单内容
 var $com;
 var MyComponent = {
   data() { return { time: "" } },
@@ -29,13 +30,16 @@ var MyComponent = {
  </div>`
 };
 let $this;
+
+//选择数据源页面
+import App_ExpertModelBody from "./App_Expert/App_ExpertModelBody.vue";
 let extension = {
   components: {
     gridHeader: '',
-    gridBody: '',
+    gridBody:'',
     gridFooter: '',
     modelHeader: '',
-    modelBody: '',
+    modelBody:  App_ExpertModelBody, //将选择数据源页面App_ExpertModelBody注册到弹出框中
     modelFooter: ''
   }, //动态扩充组件或组件路径
   buttons: { box: [] },
@@ -46,10 +50,26 @@ let extension = {
       //在表单配置的第二行后，将MyComponent组件添加到表单中
       this.editFormOptions.splice(2, 0, [{
         colSize: 12,
-        render: () => {
+        render: (h) => {
           return h(MyComponent)
         }
       }])
+
+      //选择数据源
+      this.editFormOptions.forEach(x => {
+        x.forEach(item => {
+          if (item.field == 'ExpertName') {
+            //给编辑表单设置[选择数据]操作,extra具体配置见volform组件api
+            item.extra = {
+              icon: "ios-search",
+              text: "选择数据",
+              click: item => {
+                this.$refs.modelBody.openDemo();
+              }
+            }
+          }
+        })
+      })
     },
     modelOpenAfter(row) { //打开弹出框后给组件设置不同的值
       //调用render渲染的组件
