8cb66fb7bfac256145713abaf9e46d097cdef1a6
Cookie Tang <kevin100702@hotmail.com>
2021-11-28 23:51:04 +0800

    Update VolTable.vue
    让从表编辑也可以透过hidden属性隐藏下拉选项

--- Vol.Vue/src/components/basic/VolTable.vue
+++ Vol.Vue/src/components/basic/VolTable.vue
@@ -155,6 +155,7 @@
                       v-for="(kv, kvIndex) in getSelectedOptions(column)"
                       :key="kvIndex"
                       :value="kv.key === undefined ? '' : kv.key"
+                      v-show="!kv.hidden"
                       >{{ kv.value }}</Option
                     >
                   </Select>
