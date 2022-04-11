c4c99b7cad277439d0f9525b5478223471f1bd2c
283591387@qq.com <jxx>
2022-04-11 21:31:54 +0800

    增加vue3版本编辑器自定义上传的实现

--- Vol.Vue3版本/src/components/editor/VolWangEditor.vue
+++ Vol.Vue3版本/src/components/editor/VolWangEditor.vue
@@ -14,7 +14,8 @@ export default {
     upload: {
       //上传方法
       type: Function,
-      default: (file) => {}
+      // (file, insertImgFn) => {}
+      default: null
     },
     uploadCount: {
       //最多可以上传(图片)的数量
@@ -64,6 +65,7 @@ export default {
     let editor = new E(this.$refs.volWangEditor);
     this.editor = editor;
     let $this = this;
+    editor.config.zIndex = 500;
     editor.config.height = this.height;
     editor.config.onchange = (html) => {
       this.change = false;
@@ -88,27 +90,41 @@ export default {
     editor.config.uploadImgServer = this.http.ipAddress + this.url;
     // console.log(editor.config.uploadImgServer);
     editor.config.customUploadImg = function (resultFiles, insertImgFn) {
-      let formData = new FormData();
-      let nameArr = [];
-      resultFiles.forEach(function (file) {
-        formData.append('fileInput', file, file.name);
-        nameArr.push(file.name);
-      });
-      if (!$this.url) {
-        $this.$message.error('未配置url');
-        return;
-      }
-      $this.http.post($this.url, formData, true).then((x) => {
-        if (!x.status) {
-          return $this.$message.error(x.message);
+      // 自定义上传
+      if ($this.upload){
+        console.log("调用自定义的上传方法");
+        console.log(resultFiles)
+        // resultFiles 是 input 中选中的文件列表
+        // insertImgFn 是获取图片 url 后，插入到编辑器的方法
+        //有可能会上传多张图片,上传多张图片就需要进行遍历
+        resultFiles.map((item) => {
+          // _this.getUploadImg(item, insertImgFn);
+          $this.upload(item, insertImgFn);
+        });
+      } else {
+        let formData = new FormData();
+        let nameArr = [];
+        resultFiles.forEach(function (file) {
+          formData.append('fileInput', file, file.name);
+          nameArr.push(file.name);
+        });
+        if (!$this.url) {
+          $this.$message.error('未配置url');
+          return;
         }
-        let imgs = nameArr
-          .map((m) => {
-            return $this.http.ipAddress + x.data + m;
-          })
-          .join(',');
-        insertImgFn(imgs);
-      });
+        $this.http.post($this.url, formData, true).then((x) => {
+          if (!x.status) {
+            return $this.$message.error(x.message);
+          }
+          let imgs = nameArr
+            .map((m) => {
+              return $this.http.ipAddress + x.data + m;
+            })
+            .join(',');
+          insertImgFn(imgs);
+        });
+      }
+
     };
     editor.create();
     editor.txt.html(this.modelValue);
