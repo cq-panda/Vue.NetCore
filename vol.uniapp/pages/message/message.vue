f4a65330ea455b767499d5c52155c88bef0d524d
283591387@qq.com <jxx>
2022-04-20 01:31:55 +0800

    增加uniapp示例

--- vol.uniapp/pages/message/message.vue
+++ vol.uniapp/pages/message/message.vue
@@ -5,44 +5,69 @@
 		</u-sticky>
 
 		<view class="message-list">
-			<u-list @scrolltolower="scrolltolower" :height="height" :lowerThreshold='50'>
-				<view class="message-list-item" v-for="(item,index) in list" :key="index">
-					<view class="message-list-item-left">
-						<u--image width="80px" height="80px" radius="5px" :src="item.imgs[0]"></u--image>
-					</view>
-					<view class="message-list-item-right">
-						<view class="message-list-item-right-title">
-							{{item.title}}
+			<u-skeleton rows="3" :loading="loading" avatar :title="false">
+				<u-list @scrolltolower="scrolltolower" :height="height" :lowerThreshold='50'>
+					<message-list :list="list"></message-list>
+					<view style="height: 50px;"></view>
+				</u-list>
+				<!-- <u-list @scrolltolower="scrolltolower" :height="height" :lowerThreshold='50'>
+					<view @click="toDetail(item)" class="message-list-item" v-for="(item,index) in list" :key="index">
+						<view class="message-list-item-left">
+							<u--image width="80px" height="80px" radius="5px" :src="item.imageUrl"></u--image>
 						</view>
-						<view class="message-list-item-right-small-text">
-							<view class="text">{{item.author}}</view>
-							<view class="flag" v-if="item.flag">{{item.flag}}</view>
-							<view class="text">{{item.date}}</view>
+						<view class="message-list-item-right">
+							<view class="message-list-item-right-title">
+								{{item.title}}
+							</view>
+							<view class="message-list-item-right-small-text">
+								<view class="text">{{item.creator}}</view>
+								<view class="flag" v-if="index<=3">最新</view>
+								<view class="text">{{item.createDate.substring(0,16)}}</view>
+							</view>
 						</view>
 					</view>
-				</view>
-			</u-list>
+					<view style="height: 50px;"></view>
+				</u-list> -->
+			</u-skeleton>
 		</view>
 		<vol-tabbar :index="3"></vol-tabbar>
 	</view>
 </template>
 
 <script>
+	import messagelist from './message-list.vue'
 	export default {
+		components:{
+			'message-list':messagelist
+		},
 		methods: {
 			scrolltolower() {
-				this.list.push({
-					title: "做程序员关键技术要点是什么??程序员发展前景如何?",
-					header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head1.jpg",
-					author: "路边社北京分社",
-					date: "1天前",
-					imgs: [
-						"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj01.jpg"
-					]
+				this.getList();
+			},
+			async getNav() {
+				await this.http.get('api/app_news/getNav', {}, true).then(result => {
+					this.list1 = result
+				})
+			},
+			getList() {
+				if (this.page < 0) {
+					return;
+				}
+				this.page++;
+				let url = 'api/app_news/getList?newsType=' + this.id + "&page=" + this.page;
+				this.http.get(url, {}, false).then(result => {
+					result.forEach(item => {
+						item.imageUrl = this.http.ipAddress + item.imageUrl;
+					})
+					if (!result.length) {
+						this.page = -1;
+					}
+					this.list.push(...result);
+					this.loading = false;
 				})
 			}
 		},
-		onLoad() {
+		async onLoad() {
 			uni.hideTabBar({
 				animation: false
 			})
@@ -52,94 +77,27 @@
 					_this.height = res.windowHeight - 50;
 				}
 			});
+			await this.getNav();
+			if (!this.list1.length) {
+				this.loading = false;
+				return;
+			}
+			this.id = this.list1[0].id;
+			this.getList();
+
 		},
 		data() {
 			return {
+				id: 0,
+				page: 0,
+				loading: true,
 				height: 0,
 				list1: [{
-					name: '关注',
+					name: '',
 				}, {
-					name: '推荐',
-				}, {
-					name: '电影'
-				}, {
-					name: '科技'
-				}, {
-					name: '音乐'
-				}, {
-					name: '美食'
+					name: '',
 				}],
-				list: [{
-						title: "刚刚老板向我借计算器，我说你算什么东西，然后老板让我发个红包给他道...",
-						header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head3.jpg",
-						author: "路边社消息",
-						date: "刚刚",
-						flag: "置顶",
-						link: "",
-						imgs: [
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj04.jpg"
-						]
-					}, {
-						title: "当往事已成回忆，我从千万家产到非洲当雇佣兵的日子",
-						header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head3.jpg",
-						author: "路边社非洲分社",
-						date: "1分钟前",
-						flag: "探险",
-						imgs: [
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj01.jpg"
-						]
-					},
-					{
-						title: "零點糗事的糗事：谁能拥有这样的逗比女朋友，一定是休了八百辈子的福了",
-						header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head3.jpg",
-						author: "路边社消息",
-						date: "5分钟前",
-						flag: "八卦",
-						link: "",
-						imgs: [
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj04.jpg",
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj04.jpg"
-						]
-					},
-					{
-						title: "当往事已成回忆，我从千万家产到非洲当雇佣兵的日子",
-						header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head3.jpg",
-						author: "路边社非洲分社",
-						date: "1分钟前",
-						flag: "探险",
-						imgs: [
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj01.jpg"
-						]
-					},
-					{
-						title: "现在找对象一定要看仔细一些，因为现在不男不女的人太多了！",
-						header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head3.jpg",
-						author: "路边社消息",
-						date: "1分钟前",
-						imgs: [
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj04.jpg"
-						]
-					},
-					{
-						title: "做程序员关键技术要点是什么??程序员发展前景如何?",
-						header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head1.jpg",
-						author: "路边社北京分社",
-						date: "1天前",
-						imgs: [
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj05.jpg"
-						]
-					},
-					{
-						title: "刚刚老板向我借计算器，我说你算什么东西，然后老板让我发个红包给他道...",
-						header: "https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/head3.jpg",
-						author: "路边社消息",
-						date: "1分钟前",
-						link: "",
-						imgs: [
-							"https://imgs-1256993465.cos.ap-chengdu.myqcloud.com/h5pic/tj05.jpg",
-						]
-					},
-				],
+				list: []
 			}
 		}
 	}
@@ -151,6 +109,7 @@
 
 	.message-list {
 		.message-list-item {
+			min-height: 100rpx;
 			display: flex;
 			margin: 14rpx 14rpx 0 14rpx;
 			background: white;
