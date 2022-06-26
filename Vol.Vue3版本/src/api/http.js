eb7a6fcbc7a21c70d215e6aa1d62218e428a68c2
283591387@qq.com <jxx>
2022-06-27 00:13:07 +0800

    增加通用下载文件(excel)方法this.http.download()说明

--- Vol.Vue3版本/src/api/http.js
+++ Vol.Vue3版本/src/api/http.js
@@ -148,7 +148,10 @@ function get (url, param, loading, config) {
     })
 }
 
-
+//url:url地址
+//params:请求参数
+//fileName:下载的文件名
+//loading:是否显示加载状态
 function download (url, params, fileName, loading,callback) {
     fileName = fileName.replace(">", "＞").replace("<", "＜");
     post(url, params, loading, { responseType: 'blob' }).then(content => {
