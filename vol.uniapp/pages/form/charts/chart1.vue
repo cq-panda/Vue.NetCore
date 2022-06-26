99737818609b5078b143fe77fafc0efd5828bc92
283591387@qq.com <jxx>
2022-06-27 00:25:06 +0800

    增加移动端示例

---
+++ vol.uniapp/pages/form/charts/chart1.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		整理中
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- vol.uniapp/pages/form/form.vue
+++ vol.uniapp/pages/form/form.vue
@@ -1,29 +1,22 @@
 <template>
 	<view class="v-form-container">
-		<view class="v-form-item">
-			<!-- 		<view class="v-form-title">分类</view> -->
-			<u-grid :border="false" :col="4">
-				<u-grid-item v-for="(item,index) in list" :key="index">
-					<image style="width: 50px;height: 50px;margin-top: 16rpx;" :src="item.url"></image>
-					<view class="grid-text">{{item.name}}</view>
-				</u-grid-item>
-			</u-grid>
-		</view>
+		<u-notice-bar color="#54a6ff" bgColor="#e9f3ff" text="这里是一些常用功能,后续会不断更新........."></u-notice-bar>
 
-		<view style="padding:0rpx 20rpx 0 20rpx">
-			<vol-alert type="primary">
-				<view>秋云：https://demo.ucharts.cn</view>
-			</vol-alert>
+		<view class="grid-list" v-for="(item,index) in grid">
+			<view class="grid-title">
+				<view class="grid-title-text">
+					{{item.name}}
+				</view>
+			</view>
+			<view class="grid-item" @click="gridClick(data.path)" v-for="(data,dindex) in item.data" :key="dindex">
+				<view class="grid-icon" :class="data.bg">
+					<u-icon color="#ffff" size="22" :name="data.icon"></u-icon>
+				</view>
+				<view class="grid-text">
+					{{data.name}}
+				</view>
+			</view>
 		</view>
-	<!-- 	<view class="charts-box v-form-item">
-			<qiun-data-charts type="column" style="height: 400rpx;" :chartData="columnData" :echartsH5="true"
-				:echartsApp="true" :inScrollView="true" :disableScroll="true" :canvas2d="true" :errorReload="false"
-				background="none" />
-		</view> -->
-		<view class="charts-box v-form-item">
-			<qiun-data-charts style="height: 400rpx;" type="ring" :chartData="chartData" />
-		</view>
-
 		<vol-tabbar :index="2"></vol-tabbar>
 	</view>
 </template>
@@ -32,88 +25,92 @@
 	export default {
 		data() {
 			return {
-				list: [{
-						name: "女装",
-						url: '/static/imgs/img01.png'
+				grid: [{
+						name: "Table表格",
+						data: [{
+							name: "水平显示",
+							icon: "file-text",
+							bg: "color1",
+							path: "/pages/table/table1/table1"
+						}, {
+							name: "列表显示",
+							icon: "list-dot",
+							bg: "color2",
+							path: "/pages/table/table2/table2"
+						}, {
+							name: "绑定数据源",
+							icon: "list",
+							bg: "color3",
+							path: ""
+						}, {
+							name: "格式化显示",
+							icon: "order",
+							bg: "color4",
+							path: ""
+						}, {
+							name: "行点击事件",
+							icon: "more-circle",
+							bg: "color5",
+							path: ""
+						}]
 					},
 					{
-						name: "男装",
-						url: '/static/imgs/img02.png'
+						name: "表单",
+						data: [{
+							name: "基础表单",
+							icon: "file-text",
+							bg: "color5",
+							path: "/pages/form/form2"
+						}, {
+							name: "只读表单",
+							icon: "calendar",
+							bg: "color3",
+							path: "/pages/form/form1"
+						}, {
+							name: "事件绑定",
+							icon: "pause-circle",
+							bg: "color2",
+								path: "/pages/form/form2"
+						}, {
+							name: "自定义",
+							icon: "edit-pen",
+							bg: "color4",
+							path: ""
+						}]
 					},
 					{
-						name: "奶瓶",
-						url: '/static/imgs/img03.png'
-					},
-					{
-						name: "内衣",
-						url: '/static/imgs/img04.png'
-					},
-					{
-						name: "蛋糕",
-						url: '/static/imgs/img05.png'
-					},
-					{
-						name: "灯具",
-						url: '/static/imgs/img06.png'
+						name: "地图",
+						data: [{
+							name: "地图地位",
+							icon: "map",
+							bg: "color4",
+							path: ""
+						}, {
+							name: "地图轨迹",
+							icon: "play-circle",
+							bg: "color3",
+							path: ""
+						}, {
+							name: "地图标记",
+							icon: "thumb-up",
+							bg: "color1",
+							path: ""
+						}]
 					}, {
-						name: "男装",
-						url: '/static/imgs/img02.png'
-					},
-					{
-						name: "其他",
-						url: '/static/imgs/img03.png'
+						name: "图表",
+						data: [{
+							name: "图表1",
+							icon: "share",
+							bg: "color3",
+							path: "/pages/form/charts/chart1"
+						}, {
+							name: "图表2",
+							icon: "photo",
+							bg: "color2",
+							path: ""
+						}]
 					}
-				],
-				chartData: {
-					"series": [{
-						"name": "一班",
-						"data": 50
-					}, {
-						"name": "二班",
-						"data": 30
-					}, {
-						"name": "三班",
-						"data": 20
-					}, {
-						"name": "四班",
-						"data": 18
-					}, {
-						"name": "五班",
-						"data": 8
-					}]
-				},
-				columnData: {
-					"categories": [
-						"2020",
-						"2021",
-						"2023"
-					],
-					"yAxis": {
-						"disabled": false,
-						"disableGrid": true
-					},
-					xAxis: {
-						"disableGrid": true
-					},
-					"series": [{
-							"name": "目标值",
-							"data": [
-								35,
-								36,
-								34
-							]
-						},
-						{
-							"name": "完成量",
-							"data": [
-								18,
-								27,
-								28
-							]
-						}
-					]
-				}
-
+				]
 			}
 		},
 		onShow() {
@@ -122,36 +119,102 @@
 			})
 		},
 		methods: {
-			rightClick() {}
+			gridClick(path) {
+				if (!path) {
+					this.$toast('开发中')
+					return;
+				}
+				uni.navigateTo({
+					url: path
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="less" scoped>
 	.v-form-container {
+		background: #fbfbfb;
+		height: calc(100vh - 110rpx);
+		overflow: scroll;
 		padding-bottom: 90rpx;
-		background: #f9f9f9;
 	}
 
-	.v-form-title {
-		font-size: 28rpx;
-		color: #8f9ca2;
-		padding: 4rpx 0 0rpx 20rpx;
-	}
+	.grid-list {
+		border-top: 1px solid #f9f9f9;
+		border-bottom: 1px solid #f9f9f9;
+		margin-bottom: 24rpx;
+		width: 100%;
+		display: inline-block;
+		background: #ffff;
 
-	.v-form-item {
-		// box-shadow: 1px 1px 9px #efefef;
-		margin: 20rpx;
-		border: 1px solid #f1f1f1;
-		border-radius: 10rpx;
-		padding: 20rpx 4rpx;
-		background: #FFFFFF;
+		.grid-item {
+			width: 20%;
+			text-align: center;
+			float: left;
+			padding: 20rpx 0;
+		}
+
+		.grid-title {
+			border-bottom: 1px solid #f7f7f7;
+			padding: 20rpx;
+			// margin: 10px;
+			// font-weight: bold;
+
+			.grid-title-text {
+				border-left: 16rpx solid #00aaff;
+				line-height: 1.1;
+				padding-left: 10rpx;
+
+			}
+		}
+
+		.grid-icon {
+			width: 80rpx;
+			height: 80rpx;
+			background: #eee;
+			border-radius: 34rpx;
+			position: relative;
+			left: 0;
+			right: 0;
+			margin: auto;
+		}
+
+		.color1 {
+			background-image: linear-gradient(#e8b752, #e8b752);
+		}
+
+		//
+
+		.color2 {
+			background-image: linear-gradient(#e55764, #e55764);
+		}
+
+		.color3 {
+			background-image: linear-gradient(#78c4df, #78c4df);
+		}
+
+		.color4 {
+			background-image: linear-gradient(#69a0f1, #69a0f1);
+		}
+
+		.color5 {
+			background-image: linear-gradient(#736be2, #736be2);
+		}
 
 		.grid-text {
 			font-size: 24rpx;
-			color: #4f4747;
-			margin-top: 8rpx;
-			margin-bottom: 20rpx;
+			color: #7e7e7e;
+			margin-top: 10rpx;
 		}
+	}
+</style>
+<style scoped>
+	.grid-list /deep/ .u-icon {
+		flex-direction: column !important;
+		position: absolute;
+		top: 48%;
+		left: 50%;
+		transform: translate(-50%, -50%);
 	}
 </style>
