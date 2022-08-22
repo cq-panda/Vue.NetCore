01c1e8a99b10a40db8fbcd9a1695e0189b194ba8
p1j123456@126.com <p1j123456@126.com>
2022-08-22 21:39:08 +0800

    更新

---
+++ Vue.Net/VOL.Core/WorkFlow/AuditStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VOL.Core.WorkFlow
+{
+    public enum AuditStatus
+    {
+        审核中 = 0,
+        审核通过 = 1,
+        审核未通过 = 2,
+        驳回 = 3
+    }
+
+    public enum AuditType
+    {
+        用户审批 = 1,
+        角色审批 = 2,
+        部门审批 = 3
+    }
+}
