f8689e0a1fe401b014fdd1c6e0cd9c9a874d6efb
283591387@qq.com <jxx>
2022-12-26 02:36:01 +0800

    增加数据源bigint类型的支持

--- Vol.Vue3版本/src/components/basic/ViewGrid/methods.js
+++ Vol.Vue3版本/src/components/basic/ViewGrid/methods.js
@@ -517,7 +517,7 @@ let methods = {
             });
             if (treeDic && treeDic.orginData && treeDic.orginData.length) {
               if (typeof treeDic.orginData[0].id == 'number') {
-                newVal = ~~newVal;
+                newVal = newVal*1||0;
               } else {
                 newVal = newVal + '';
               }
