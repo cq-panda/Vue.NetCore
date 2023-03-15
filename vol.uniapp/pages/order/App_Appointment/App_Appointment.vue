58a9a9c62b52828738a6a684784e8d6ce5afd714
jxx <283591387@qq.com>
2023-03-15 11:30:31 +0800

    增加移动端自动生成页面的扫描搜索功能

--- vol.uniapp/pages/order/App_Appointment/App_Appointment.vue
+++ vol.uniapp/pages/order/App_Appointment/App_Appointment.vue
@@ -3,16 +3,20 @@
 		<view-grid ref="grid" @delTest="delTest" :index="true" :options="options">
 			<!-- 自定义slot -->
 			<view slot="gridHeader" class="grid-header">
-				{{gridHeaderText}}
 				<vol-alert>
 					<view>1、页面由代码生成器生成,自动控制前后端权限</view>
 					<view>2、当前页面在后台菜单上配置的只读不能添加与修改</view>
+					<view>3、点击搜索框扫描图标可以扫描搜索</view>
+					<view>4、示例代码见App_Appointment.vue文件</view>
 				</vol-alert>
+				<view style="padding: 20rpx;border-bottom: 1px solid #f5f3f3;">
+					<u-search @search="searchInputClick" @custom="searchInputClick" placeholder="请输入电话搜索"
+						:showAction="true" actionText="搜索" searchIcon="scan" @clickIcon="scanClick" :animation="false"
+						v-model="searchText">
+					</u-search>
+				</view>
 			</view>
 		</view-grid>
-
-	
-
 	</view>
 </template>
 
@@ -29,7 +33,7 @@
 	export default {
 		data() {
 			return {
-				gridHeaderText: "", //随便输入字符后可以显示出来
+				searchText: "",
 				options: _options,
 				show: false,
 				row: {}
@@ -37,7 +41,19 @@
 		},
 		onShow() {},
 		methods: {
-			
+			scanClick() {
+				//扫一扫点击事件
+				uni.scanCode({
+					success: (res) => {
+						this.searchText = res.result;
+						this.searchInputClick();
+					}
+				})
+			},
+			searchInputClick() { //点击搜索
+				//生成页面的搜索功能，见App_AppointmentExtend.js中searchInputClick操作
+				this.$refs.grid.searchInputClick(this.searchText);
+			},
 		}
 	}
 </script>
