b606d847d83d6c6ea22a351910955ff552b2b52d
283591387@qq.com <jxx>
2023-04-10 00:24:35 +0800

    优化vue3版本前端权限按钮判断

--- 开发版dev/Vue.NetCore/Vol.Vue3版本/src/api/permission.js
+++ 开发版dev/Vue.NetCore/Vol.Vue3版本/src/api/permission.js
@@ -7,28 +7,38 @@ let permission = {
     getMenu() {
         return http.get("/api/getTreeMenu");
     }, getButtons(path, extra, table, tableName) {//extra自定额外按钮
-        //table获取指定表的权限
-        if (table) { table = '/' + table; }
-        let permission = store.getters.getPermission(table || path);
+       //extra自定额外按钮
+    //table获取指定表的权限
+    if (table) {
+        table = '/' + table;
+      }
+      let permission = store.getters.getPermission(table || path);
+      if (!permission) {
+        permission = store.getters.getPermission(path.substring(1));
         if (!permission) {
-            permission = store.getters.getPermission(path.substring(1));
+          if ((tableName || '').indexOf('/') != -1) {
+            let arr = tableName.split('/');
+            tableName = arr[arr.length - 1];
+          }
+          permission = store.getters.getPermission('/' + tableName);
+          if (!permission) {
+            permission = (store.state.permission||[]).find(x => x.tableName ==tableName);
             if (!permission) {
-                permission = store.getters.getPermission("/" + tableName);
-                if (!permission) {
-                    to401();
-                    return;
-                }
+              to401();
+              return;
             }
+          }
         }
-
-        let permissions = permission.permission;//.split(',');
-        let gridButtons = buttons.filter(item => {
-            return !item.value || permissions.indexOf(item.value) != -1
-        });
-        if (extra && extra instanceof Array) {
-            gridButtons.push(...extra)
-        }
-        return gridButtons;
+      }
+  
+      let permissions = permission.permission; //.split(',');
+      let gridButtons = buttons.filter((item) => {
+        return !item.value || permissions.indexOf(item.value) != -1;
+      });
+      if (extra && extra instanceof Array) {
+        gridButtons.push(...extra);
+      }
+      return gridButtons;
     }, to401() {
         to401();
     }
