b1a41d16b597369057b429a85fb5be6160e61d5a
jxx <283591387@qq.com>
2023-04-19 15:40:28 +0800

    重写vue3版本vol-box自适应

--- Vol.Vue3版本/src/components/basic/VolBox.vue
+++ Vol.Vue3版本/src/components/basic/VolBox.vue
@@ -1,40 +1,37 @@
 <template>
-  <el-dialog
-    custom-class="v-dialog"
-    v-model="vmodel"
-    :top="top"
-    :close-on-click-modal="false"
-    :close-on-press-escape="false"
-    :width="width"
-    :draggable="draggable"
-    :modal="modal"
-    :before-close="handleClose"
-  >
-    <template #header> <i :class="icon"></i> {{ title }} </template>
-    <div class="dia-content" :style="{ height: contentHeight + 'px' }">
-      <el-scrollbar style="flex: 1">
-        <!-- 是否开启懒加载2020.12.06 -->
-        <div
-          v-if="inited"
-          class="srcoll-content"
-          :style="{ padding: padding + 'px' }"
-        >
+  <div class="vol-dialog">
+    <!-- <el-dialog v-model="vmodel" :title="title" align-center :close-on-click-modal="false" :close-on-press-escape="false"
+      :width="width" :draggable="draggable" :modal="modal" :before-close="handleClose">
+      <span>Open the dialog from the center from the screen</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="centerDialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="centerDialogVisible = false">
+            Confirm
+          </el-button>
+        </span>
+      </template>
+    </el-dialog> -->
+
+    <el-dialog v-model="vmodel" :align-center="true" :close-on-click-modal="false" :close-on-press-escape="false"
+      :width="width" :draggable="draggable" :modal="modal" :before-close="handleClose">
+      <template #header> <i :class="icon"></i> {{ title }} </template>
+      <el-scrollbar :max-height="contentHeight">
+
+        <div v-if="inited" class="srcoll-content" :style="{ padding: padding + 'px' }">
           <slot name="content"></slot>
           <slot></slot>
         </div>
       </el-scrollbar>
-      <div class="dia-footer">
-        <slot name="footer"></slot>
-        <el-button
-          type="primary"
-          v-if="!footer"
-          size="mini"
-          @click="handleClose()"
-          ><i class="el-icon-close"></i>关闭</el-button
-        >
-      </div>
-    </div>
-  </el-dialog>
+      <template #footer>
+        <div class="dia-footer">
+          <slot name="footer"></slot>
+          <el-button type="primary" v-if="!footer" size="mini" @click="handleClose()"><i
+              class="el-icon-close"></i>关闭</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -90,7 +87,7 @@ export default defineComponent({
 
   },
   setup(props, context) {
-    const clientHeight = document.body.clientHeight;
+    const clientHeight = document.body.clientHeight * 0.95 - 60;
     const inited = ref(true);
     const vmodel = ref(false);
     const footer = ref(false);
@@ -142,15 +139,54 @@ export default defineComponent({
 </script>
 
 <style lang="less" scoped>
-.dia-content {
-  display: flex;
-  flex-direction: column;
-  .dia-footer {
-    min-height: 40px;
-    width: 100%;
-    border-top: 1px solid #e2e2e2;
-    text-align: right;
-    padding: 6px 8px;
-  }
+.dia-footer {
+  text-align: right;
+  width: 100%;
+  border-top: 1px solid #e2e2e2;
+  text-align: right;
+  padding: 6px 8px;
+}
+</style>
+
+<style scoped lang="less">
+.vol-dialog ::v-deep(.el-overlay-dialog) {
+  display: flex !important;
+}
+
+.vol-dialog ::v-deep(.el-dialog) {
+  margin: auto;
+}
+
+.vol-dialog ::v-deep(.el-dialog) {
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+}
+
+.vol-dialog ::v-deep(.el-dialog__header) {
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  padding: 0px 13px;
+  line-height: 53px;
+  border-bottom: 1px solid #e6e6e6;
+  height: 50px;
+  color: white;
+  font-weight: bold;
+  font-size: 14px;
+  margin: 0;
+  background-image: linear-gradient(135deg, #0cd7bd 10%, #50c3f7);
+}
+
+.vol-dialog ::v-deep(.el-dialog__footer),
+.vol-dialog ::v-deep(.el-dialog__body) {
+  padding: 0;
+}
+
+.vol-dialog ::v-deep(.el-dialog__headerbtn) {
+  top: 0;
+  padding-top: 8px;
+  height: 50px;
+}
+.vol-dialog ::v-deep(.el-dialog__headerbtn .el-dialog__close) {
+  color: #fff;
 }
 </style>
