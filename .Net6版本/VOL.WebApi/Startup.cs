50b12d7a9ab23258e1db899446739f520500cbff
283591387@qq.com <jxx>
2023-05-15 22:57:03 +0800

    增加.net6审批流程启动示例

--- .Net6版本/VOL.WebApi/Startup.cs
+++ .Net6版本/VOL.WebApi/Startup.cs
@@ -193,8 +193,15 @@ namespace VOL.WebApi
         public void ConfigureContainer(ContainerBuilder builder)
         {
             Services.AddModule(builder, Configuration);
-            //初始化流程表，表里面必须有AuditStatus字段 
-            WorkFlowContainer.Instance.Use<App_Expert>().Use<SellOrder>();
+            //初始化流程表，表里面必须有AuditStatus字段
+            WorkFlowContainer.Instance
+                //name= 流程实例名称
+                //filterFields流程实例名称
+                .Use<SellOrder>(name: "订单管理", filterFields: x => new { x.OrderType, x.Qty, x.CreateID, x.SellNo })
+
+                .Use<App_Expert>()
+                //run方法必须写在最后位置
+                .Run();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
