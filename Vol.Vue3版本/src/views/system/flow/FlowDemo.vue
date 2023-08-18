03b2faf70688993ca5dc514eef6e1087bc4e3d41
jxx <283591387@qq.com>
2023-08-18 17:42:00 +0800

    增加发起流程页面(功能开发中)

--- Vol.Vue3版本/src/views/system/flow/FlowDemo.vue
+++ Vol.Vue3版本/src/views/system/flow/FlowDemo.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="flow-demo">
-    <el-divider>发起流程</el-divider>
-
-    <vol-form
-      style="margin-top:20px;"
-      ref="form"
-      :load-key="true"
-      :label-width="60"
-      :formRules="options"
-      :formFields="fields"
-    ></vol-form>
-    <div style="text-align:center">
-      <el-button @click="submit" type="primary">提交</el-button>
-    </div>
+  <div class="home-content" data-v-7f6868a7="">
+    <el-scrollbar style="height: 100%;">
+      <div style="margin:10px 0 -5px 12px;font-size: 14px;font-weight: bold;">
+        <i class="el-icon-warning-outline"></i> 发起流程
+        <a class="contact" target="_blank">
+          发起流程功能正在开发中。目前已实现从表单数据中处理流程</a>
+      </div>
+      <div class="home-list">
+        <div class="list-item" @click="itemClick(item,index)" v-for="(item, index) in list" :key="index">
+          <div class="content">
+            <div class="content-right">
+              <div class="name">
+                <i class="el-icon-warning-outline"></i>{{ item.name }}
+              </div>
+              <div class="data" data-v-7f2e9c68="">
+                {{ item.desc }}
+              </div>
+            </div>
+            <div class="mouse-enter-class"></div>
+          </div>
+          <div :class="[item.type == '审批' ? 'item-strengthen' : (item.type == '新增' ? 'item-new' : 'item-other')]">{{ item.type }}</div>
+        </div>
+      </div>
+    </el-scrollbar>
   </div>
 </template>
 <script>
@@ -21,84 +31,225 @@ import {
   ref,
   reactive,
   toRefs,
-  getCurrentInstance
+  getCurrentInstance,
+  onMounted,
+  onUnmounted
 } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import http from '@/../src/api/http.js';
-import VolForm from '@/components/basic/VolForm.vue';
 export default {
-  components: {
-    VolForm
-  },
-  setup() {
-    const fields = reactive({
-      ExpertName: '',
-      Enable: '',
-      City: '',
-      IDNumber: '',
-      PhoneNo: '',
-      CreateDate: '',
-      Resume: '',
-      HeadImageUrl: ''
-    });
-    const options = reactive([
-      [{ title: '名称', field: 'ExpertName' }],
-      [
-        {
-          dataKey: 'enable',
-          data: [],
-          title: '状态',
-          required: true,
-          field: 'Enable',
-          type: 'select'
-        }
-      ],
-      [
-        {
-          dataKey: 'city',
-          data: [],
-          title: '地区',
-          field: 'City',
-          type: 'select'
-        }
-      ],
-      [{ title: '日期', field: 'CreateDate', type:"date" }],
-      [{ title: '备注', field: 'Resume', colSize: 12, type: 'textarea' }]
+  components: {},
+  setup(props) {
+    const list = reactive([
+      {
+        name: '订单审批',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      {
+        name: '请假管理',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      { name: '物资申请',  desc: '这里是一些不可描述的信息...', type: "审批" },
+      { name: '采购申请',  desc: '这里是一些不可描述的信息...',type: "审批" },
+      {
+        name: '合同申请',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      {
+        name: '订单审批',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      {
+        name: '请假管理',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      { name: '物资申请',  desc: '这里是一些不可描述的信息...', type: "审批" },
+      { name: '采购申请',  desc: '这里是一些不可描述的信息...',type: "审批" },
+      {
+        name: '合同申请',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      {
+        name: '订单审批',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      {
+        name: '请假管理',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      { name: '物资申请',  desc: '这里是一些不可描述的信息...', type: "审批" },
+      { name: '采购申请',  desc: '这里是一些不可描述的信息...',type: "审批" },
+      {
+        name: '合同申请',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },    {
+        name: '订单审批',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      {
+        name: '请假管理',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      },
+      { name: '物资申请',  desc: '这里是一些不可描述的信息...', type: "审批" },
+      { name: '采购申请',  desc: '这里是一些不可描述的信息...',type: "审批" },
+      {
+        name: '合同申请',
+        desc: '这里是一些不可描述的信息...',
+        type: "审批"
+      }
     ]);
-    let appContext = getCurrentInstance().appContext;
-    let $message = appContext.config.globalProperties.$message;
-    const form = ref(null);
-
-    const submit = () => {
-      form.value.validate(() => {
-        http
-          .post('api/App_Expert/add', { mainData: fields }, true)
-          .then((result) => {
-            if (!result.status) {
-              return $message.error(result.message);
-            }
-            form.value.reset();
-            $message.success(result.message);
-          });
-      });
-    };
-
+    onMounted(() => {
+   
+    });
+    const {proxy}=getCurrentInstance();
+    const itemClick=(item)=>{
+           proxy.$message.success('开发中..')
+    }
     return {
-      options,
-      fields,
-      submit,
-      form
+      list,
+      itemClick
     };
   }
 };
 </script>
-<style scoped lang="less">
-.flow-demo {
+<style lang="less" scoped>
+// @import './home/index.less';
+
+.home-content {
   position: absolute;
-  width: 600px;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-  margin-top: 20px;
+  height: 100%;
+  width: 100%;
+  background: #f3f7fb;
+
+  .home-list {
+    margin: 12px;
+    display: grid;
+    -moz-column-gap: 12px;
+    column-gap: 12px;
+    grid-template-columns: repeat(5, auto);
+  }
+
+  .list-item {
+    position: relative;
+    cursor: pointer;
+    margin-bottom: 12px;
+    transition: transform 0.8s;
+    .content {
+      position: relative;
+      height: 110px;
+      // padding-left: 40px;
+      background: #ffffff;
+      display: flex;
+      align-items: center;
+      transition: all 1.5s;
+      border-radius: 5px;
+      overflow: hidden;
+
+      .content-right {
+        color: #1d252f;
+        padding: 0 20px;
+
+        .el-icon-warning-outline {
+          margin-right: 5px;
+        }
+      }
+
+      .name {
+        transition: transform .5s;
+        color: #060606;
+        font-size: 16px;
+        font-weight: 400;
+        padding-bottom: 5px;
+      }
+
+      .data {
+        font-size: 12px;
+        font-family: Source Han Sans CN, sans-serif;
+        color: #6f6f6f;
+      }
+    }
+
+    .mouse-enter-class {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 0;
+      height: 40px;
+      border-bottom: 3px #0763ee solid;
+      transition: 1s;
+    }
+
+ 
+  }
 }
-</style>
+
+.list-item:hover {
+
+  transform: scale(1.04);
+  .content {
+    background: #ecf5f9;
+
+    .mouse-enter-class {
+      width: 100%;
+    }
+
+  }
+}
+
+
+.home-list-content {
+  margin: -12px 12px;
+  background: #ffff;
+  padding: 20px;
+  display: flex;
+  margin-bottom: 12px;
+}
+
+.contact {
+  cursor: pointer;
+  font-size: 12px;
+  font-weight: 500;
+  margin-left: 24px;
+  color: #8e8888;
+
+  img {
+    height: 15px;
+    margin-bottom: -3px;
+    margin-right: 5px;
+  }
+}
+
+.item-strengthen,
+.item-new,
+.item-other {
+  position: absolute;
+  top: 0;
+  right: 0;
+  font-size: 12px;
+  padding: 2px 10px;
+  background: #daf3ff;
+  border-bottom-left-radius: 6px;
+  color: #339aed;
+  border-top-right-radius: 5px;
+}
+
+.item-new {
+  background: #ffebe9;
+  color: #f94638;
+}
+
+.item-other {
+  background: #e1fae2;
+  color: #2ad431;
+}</style>
