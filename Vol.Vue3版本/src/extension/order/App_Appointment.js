c2390601ce5ff8b7e25f2f0406ff687bf0b0c02b
jxx <283591387@qq.com>
2023-08-18 17:18:38 +0800

    增国表格单元格合并示例

--- Vol.Vue3版本/src/extension/order/App_Appointment.js
+++ Vol.Vue3版本/src/extension/order/App_Appointment.js
@@ -13,10 +13,10 @@ let extension = {
             show-icon
           >
             <p v-html="1、 onInit(){ this.setFiexdSearchForm(true){设置固定显示所有查询条件}">
-              {' '}
             </p>
             <p v-html="2、自定义表格按钮、弹出框提示、弹出框分组信息,见示例：App_Appointment.js文件">
-              {' '}
+            </p>
+            <p v-html="3、2023.08.19集成表格行、列合并功能,更新文件见:http://v2.volcore.xyz/document/log">
             </p>
           </el-alert>
         );
@@ -77,13 +77,13 @@ let extension = {
                 查看
               </el-button>
 
-             {/* 通过条件判断,要显示的按钮 */}
-             {
-              /*  {
-                    index % 2 === 1 
-                    ?<el-button>修改</el-button>
-                    : <el-button>设置</el-button>
-                } */
+              {/* 通过条件判断,要显示的按钮 */}
+              {
+                /*  {
+                      index % 2 === 1 
+                      ?<el-button>修改</el-button>
+                      : <el-button>设置</el-button>
+                  } */
               }
 
 
@@ -160,7 +160,7 @@ let extension = {
       this.columns.forEach((col) => {
         if (col.field == 'Describe') {
           col.title = '单元格Tooltip(鼠标放上来看效果)';
-          col.showOverflowTooltip=true;
+          col.showOverflowTooltip = true;
           //或者用下面这的个方法自定义提示
           // col.render = (h, { row, column, index }) => {
           //   return (
@@ -216,36 +216,40 @@ let extension = {
       //格式化单格颜色
       this.columns.forEach((x) => {
         if (x.field == 'PhoneNo') {
+          x.title='设置背景颜色'
           x.cellStyle = (row, rowIndex, columnIndex) => {
-            if (row.PhoneNo == '138888887698' && rowIndex == 0) {
+            if (row.PhoneNo == '138888887698') { //&& rowIndex == 0
               return { background: '#ddecfd' };
+            }else  if (row.PhoneNo == '138888887692') { //&& rowIndex == 0
+              return { background: '#a9d6f9' };
             }
           };
         }
         if (x.field == 'Creator') {
+          x.title='合并列'
           x.cellStyle = (row, rowIndex, columnIndex) => {
             if (row.Creator == '超级管理员') {
               return { background: 'rgb(45 140 240)', color: '#ffff' };
             }
           };
         }
-        
+
         //设置进度条
         //进度条组件见:https://element-plus.gitee.io/zh-CN/component/progress.html#%E8%BF%9B%E5%BA%A6%E6%9D%A1%E5%86%85%E6%98%BE%E7%A4%BA%E7%99%BE%E5%88%86%E6%AF%94%E6%A0%87%E8%AF%86
         if (x.field == 'CreateDate') {
           x.title = '进度条'
-          x.render= (h, { row, column, index }) => {
-            if (index%2===1) {
+          x.render = (h, { row, column, index }) => {
+            if (index % 2 === 1) {
               //90改为row.字段
-              return  <el-progress stroke-width={4} percentage={90} />
+              return <el-progress stroke-width={4} percentage={90} />
             }
             //10改为row.字段
-            return  <el-progress stroke-width={4} color="#67c23a" show-text={true}	 percentage={10} />
+            return <el-progress stroke-width={4} color="#67c23a" show-text={true} percentage={10} />
           }
         }
 
         if (x.field == 'Name') {
-          x.title = '点击查看表单';
+          x.title = '点击查看(合并行)';
         }
       });
 
@@ -355,6 +359,29 @@ let extension = {
       }
       //移除快捷查询
       this.singleSearch = null;
+    },
+    spanMethod({ row, column, rowIndex, columnIndex }) { //2023.08.19增加行、列合并功能,el合并文档见https://element-plus.gitee.io/zh-CN/component/table.html#%E5%90%88%E5%B9%B6%E8%A1%8C%E6%88%96%E5%88%97
+      if (rowIndex % 2 === 0) {
+        if (columnIndex === 6) {
+          return [1, 2]
+        } 
+        else if (columnIndex === 5) {
+           return [0, 0]
+        }
+      }
+      if (columnIndex === 1) {
+        if (rowIndex % 2 === 0) {
+          return {
+            rowspan: 2,
+            colspan: 1,
+          }
+        } else {
+          return {
+            rowspan: 0,
+            colspan: 0,
+          }
+        }
+      }
     }
   }
 };
