61f3fb2270fce6d213482596d9a7dd061946bafc
jxx <283591387@qq.com>
2023-09-07 12:00:12 +0800

    提交actionlog文件

---
+++ Vue.Net/VOL.Core/Middleware/ActionLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOL.Core.Middleware
+{
+    public class ActionLog : Attribute
+    {
+        public string LogType { get; set; }
+        /// <summary>
+        /// 是否写入日志
+        /// </summary>
+        public bool Write { get; set; }
+        public ActionLog() : this(true)
+        {
+
+        }
+        public ActionLog(bool write = true)
+        {
+            Write = write;
+        }
+        public ActionLog(string logType)
+        {
+            LogType = logType;
+            Write = true;
+        }
+
+    }
+}
