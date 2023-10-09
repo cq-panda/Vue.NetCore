a25e16f29a2e38e3926183588102371749a50c1d
jxx <283591387@qq.com>
2023-10-09 13:43:53 +0800

    增加voltable加载方法load异步的支持及自定义url

--- 开发版dev/Vol.Vue3版本/src/components/basic/VolTable.vue
+++ 开发版dev/Vol.Vue3版本/src/components/basic/VolTable.vue
@@ -1332,7 +1332,7 @@ export default defineComponent({
           this.$emit('dicInited', dic);
         });
     },
-    async load(query, isResetPage) {
+    load(query, isResetPage) {
       // isResetPage重置分页数据
       if (!this.url) return;
       if (isResetPage) {
@@ -1365,7 +1365,9 @@ export default defineComponent({
         param.wheres = JSON.stringify(param.wheres);
       }
       this.loading = true;
-      await this.http.post(this.url, param).then(
+      let url=param.url||this.url;
+      param.url=undefined;
+      this.http.post(url, param).then(
         (data) => {
           //2021.06.04修复tree不刷新的问题
           if (this.rowKey) {
