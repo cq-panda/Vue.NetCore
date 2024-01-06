487fdb7c6c8078b6383e03f4ec744fa2d0dd7367
283591387@qq.com <jxx>
2024-01-06 18:23:28 +0800

    增加图片预览

---
+++ Vol.Vue3版本/src/components/basic/VolImageViewer.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-image-viewer hide-on-click-modal v-if="showImageViewer" :initial-index="initialIndex" :url-list="imageViewerList"
+        @close="closeViewer"></el-image-viewer>
+</template>
+<script>
+import { ref } from 'vue';
+export default {
+    setup() {
+        const initialIndex = ref(0);
+        const imageViewerList = ref([]);
+        const showImageViewer = ref(false)
+
+        const show = (imgs, index) => {
+            initialIndex.value = index || 0;
+            if (Array.isArray(imgs)) {
+                imageViewerList.value = imgs;
+            } else {
+                imageViewerList.value = [imgs]
+            }
+            showImageViewer.value=true;
+        }
+        const closeViewer=()=>{
+            showImageViewer.value=false;
+        }
+        return {
+            initialIndex,
+            imageViewerList,
+            showImageViewer,
+            closeViewer,
+            show
+        }
+    }
+}
+</script>
