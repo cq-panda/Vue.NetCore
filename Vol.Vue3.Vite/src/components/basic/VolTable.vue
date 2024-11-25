f46fcaeebd3d7f9ceea277eea039fbdeccff0861
jxx <283591387@qq.com>
2024-11-25 14:11:33 +0800

    1

--- Vol.Vue3.Vite/src/components/basic/VolTable.vue
+++ Vol.Vue3.Vite/src/components/basic/VolTable.vue
@@ -1446,6 +1446,7 @@ export default defineComponent({
             rows,
             (result) => {
               status = result
+              rows=data.rows;
             },
             data
           )
