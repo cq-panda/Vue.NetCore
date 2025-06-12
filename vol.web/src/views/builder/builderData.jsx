d0fd2794ad3afbb0ed89ca13f554f6f057cc4db4
jxx <283591387@qq.com>
2025-06-12 15:44:57 +0800

    增加代码生成器提示

--- vol.web/src/views/builder/builderData.jsx
+++ vol.web/src/views/builder/builderData.jsx
@@ -92,8 +92,9 @@ let data = {
           required: true,
           type: 'number',
           placeholder: '放在【代码生成配置】列表的文件夹ID下,如果填入【0】就是一级目录'
-        }],
-        [
+        }
+      ],
+      [
         {
           title: '项目类库',
           field: 'namespace',
@@ -109,13 +110,42 @@ let data = {
           field: 'columnCNName',
           required: true,
           placeholder: '表对应的中文名字,界面上显示会用到'
-        }],
-        [
+        }
+      ],
+      [
         {
           title: '实际表名',
           field: 'tableName',
           required: true,
-          placeholder: '数据库实际表名或者视图名(多表关联请创建视图再生成代码)'
+          placeholder: '数据库实际表名或者视图名(多表关联请创建视图再生成代码)',
+          labelRender: (h, {}) => {
+            return (
+              <div>
+                <el-tooltip placement="top-start" title="" trigger="hover">
+                  {{
+                    default: () => {
+                      return (
+                        <span>
+                          实际表名
+                          <i
+                            style="font-size:12px;margin-left:3px"
+                            class="el-icon-warning-outline"
+                          ></i>
+                        </span>
+                      )
+                    },
+                    content: () => {
+                      return (
+                        <div>
+                          数据库实际表名或者视图名(多表关联请创建视图再生成代码);如果只是创建目录，表名写一个不存在的名字并且没有填过这个名字
+                        </div>
+                      )
+                    }
+                  }}
+                </el-tooltip>
+              </div>
+            )
+          }
         }
       ],
       [
@@ -124,7 +154,35 @@ let data = {
           placeholder:
             '生成文件所在类库中的文件夹名(文件夹可以不存在);注意只需要填写文件夹名，不是路径',
           field: 'folderName',
-          required: true
+          required: true,
+          labelRender: (h, {}) => {
+            return (
+              <div>
+                <el-tooltip placement="top-start" title="" trigger="hover">
+                  {{
+                    default: () => {
+                      return (
+                        <span>
+                          文件夹名
+                          <i
+                            style="font-size:12px;margin-left:3px"
+                            class="el-icon-warning-outline"
+                          ></i>
+                        </span>
+                      )
+                    },
+                    content: () => {
+                      return (
+                        <div>
+                          生成文件所在类库中的文件夹名(文件夹可以不存在);注意只需要填写文件夹名，不是路径
+                        </div>
+                      )
+                    }
+                  }}
+                </el-tooltip>
+              </div>
+            )
+          }
         }
       ]
     ],
@@ -186,7 +244,31 @@ let data = {
         {
           title: '快捷编辑',
           field: 'expressField',
-          placeholder: '快捷编辑字段'
+          placeholder: '快捷编辑字段',
+          labelRender: (h, {}) => {
+            return (
+              <div>
+                <el-tooltip placement="top-start" title="" trigger="hover">
+                  {{
+                    default: () => {
+                      return (
+                        <span>
+                          快捷编辑
+                          <i
+                            style="font-size:12px;margin-left:3px"
+                            class="el-icon-warning-outline"
+                          ></i>
+                        </span>
+                      )
+                    },
+                    content: () => {
+                      return <div>生成的表格数据，点击此字段可进行编辑</div>
+                    }
+                  }}
+                </el-tooltip>
+              </div>
+            )
+          }
         },
         {
           title: '排序字段',
@@ -196,20 +278,75 @@ let data = {
       ],
 
       [
-
         {
           title: 'Vue路径',
           field: 'vuePath',
           type: 'text',
           placeholder: '路径：E:/app/src/views',
-          colSize: 6
+          colSize: 6,
+          labelRender: (h, {}) => {
+            return (
+              <div>
+                <el-tooltip placement="top-start" title="" width="600" trigger="hover">
+                  {{
+                    default: () => {
+                      return (
+                        <span>
+                          Vue路径
+                          <i
+                            style="font-size:12px;margin-left:3px"
+                            class="el-icon-warning-outline"
+                          ></i>
+                        </span>
+                      )
+                    },
+                    content: () => {
+                      return (
+                        <div>
+                          生成前端文件所有在绝对路径：E:/xxxx/vol.web/src/views(注意是到前端项目的views目录)
+                        </div>
+                      )
+                    }
+                  }}
+                </el-tooltip>
+              </div>
+            )
+          }
         },
         {
           title: 'app路径',
           field: 'appPath',
           type: 'text',
           placeholder: '路径：E:/uniapp/pages',
-          colSize: 6
+          colSize: 6,
+          labelRender: (h, {}) => {
+            return (
+              <div>
+                <el-tooltip placement="top-start" title="" width="600" trigger="hover">
+                  {{
+                    default: () => {
+                      return (
+                        <span>
+                          app路径
+                          <i
+                            style="font-size:12px;margin-left:3px"
+                            class="el-icon-warning-outline"
+                          ></i>
+                        </span>
+                      )
+                    },
+                    content: () => {
+                      return (
+                        <div>
+                          生成app文件所有在绝对路径：E:/xxxx/vol.app.next/pages(注意是到前端项目的pages目录)
+                        </div>
+                      )
+                    }
+                  }}
+                </el-tooltip>
+              </div>
+            )
+          }
         }
       ]
     ]
@@ -284,14 +421,58 @@ let data = {
       title: '查询行',
       width: 90,
       align: 'left',
-      edit: { type: 'text' }
+      edit: { type: 'text' },
+      renderHeader: (h, {}) => {
+        return (
+          <div>
+            <el-tooltip placement="top-start" title="" trigger="hover">
+              {{
+                default: () => {
+                  return (
+                    <span>
+                      查询行
+                      <i style="font-size:12px;margin-left:3px" class="el-icon-warning-outline"></i>
+                    </span>
+                  )
+                },
+                content: () => {
+                  return (
+                    <div>查询表单的所在第几行，只能输入数字，数字任意，相同的值会显示在一行</div>
+                  )
+                }
+              }}
+            </el-tooltip>
+          </div>
+        )
+      }
     },
     {
       field: 'searchColNo',
       title: '查询列',
       width: 90,
       align: 'left',
-      edit: { type: 'text' }
+      edit: { type: 'text' },
+      renderHeader: (h, {}) => {
+        return (
+          <div>
+            <el-tooltip placement="top-start" title="" trigger="hover">
+              {{
+                default: () => {
+                  return (
+                    <span>
+                      查询列
+                      <i style="font-size:12px;margin-left:3px" class="el-icon-warning-outline"></i>
+                    </span>
+                  )
+                },
+                content: () => {
+                  return <div>查询表单的所在第几行、第几列</div>
+                }
+              }}
+            </el-tooltip>
+          </div>
+        )
+      }
     },
     {
       field: 'searchType',
@@ -306,14 +487,60 @@ let data = {
       title: '编辑行',
       width: 90,
       align: 'numberbox',
-      edit: { type: 'text' }
+      edit: { type: 'text' },
+      renderHeader: (h, {}) => {
+        return (
+          <div>
+            <el-tooltip placement="top-start" title="" trigger="hover">
+              {{
+                default: () => {
+                  return (
+                    <span>
+                      编辑行
+                      <i style="font-size:12px;margin-left:3px" class="el-icon-warning-outline"></i>
+                    </span>
+                  )
+                },
+                content: () => {
+                  return (
+                    <div>
+                      弹出框表单字段所在第几行，只能输入数字，数字任意，相同的值会显示在一行(注意值需要大于>0才会在编辑表单显示，如果字典不需要编辑请设置为0)
+                    </div>
+                  )
+                }
+              }}
+            </el-tooltip>
+          </div>
+        )
+      }
     },
     {
       field: 'editColNo',
       title: '编辑列',
       width: 90,
       align: 'numberbox',
-      edit: { type: 'text' }
+      edit: { type: 'text' },
+      renderHeader: (h, {}) => {
+        return (
+          <div>
+            <el-tooltip placement="top-start" title="" trigger="hover">
+              {{
+                default: () => {
+                  return (
+                    <span>
+                      编辑列
+                      <i style="font-size:12px;margin-left:3px" class="el-icon-warning-outline"></i>
+                    </span>
+                  )
+                },
+                content: () => {
+                  return <div>弹出框编辑表单的所在第几行、第几列</div>
+                }
+              }}
+            </el-tooltip>
+          </div>
+        )
+      }
     },
     {
       field: 'editType',
@@ -329,7 +556,28 @@ let data = {
       width: 120,
       align: 'left',
       bind: { data: [] },
-      edit: { type: 'select', data: [] }
+      edit: { type: 'select', data: [] },
+      renderHeader: (h, {}) => {
+        return (
+          <div>
+            <el-tooltip placement="top-start" title="" trigger="hover">
+              {{
+                default: () => {
+                  return (
+                    <span>
+                      数据源
+                      <i style="font-size:12px;margin-left:3px" class="el-icon-warning-outline"></i>
+                    </span>
+                  )
+                },
+                content: () => {
+                  return <div>请先在数据字典维护数据源，然后刷新页面</div>
+                }
+              }}
+            </el-tooltip>
+          </div>
+        )
+      }
     },
     {
       field: 'isImage',
@@ -375,13 +623,13 @@ let data = {
       align: 'left',
       edit: { type: 'switch', keep: true }
     },
-    {
-      field: 'isColumnData',
-      title: '数据列',
-      width: 120,
-      align: 'left',
-      edit: { type: 'switch', keep: true }
-    },
+    // {
+    //   field: 'isColumnData',
+    //   title: '数据列',
+    //   width: 120,
+    //   align: 'left',
+    //   edit: { type: 'switch', keep: true }
+    // },
     {
       field: 'isDisplay',
       title: '是否显示',
@@ -413,9 +661,34 @@ let data = {
           { key: 10, value: '80%' },
           { key: 12, value: '100%' }
         ]
+      },
+      renderHeader: (h, {}) => {
+        return (
+          <div>
+            <el-tooltip placement="top-start" title="" trigger="hover">
+              {{
+                default: () => {
+                  return (
+                    <span>
+                      编辑字段宽度
+                      <i style="font-size:12px;margin-left:3px" class="el-icon-warning-outline"></i>
+                    </span>
+                  )
+                },
+                content: () => {
+                  return (
+                    <div>
+                      编辑表单的宽度，也可手动设置：http://v3.volcore.xyz/docs/web/form/inputWidth.html
+                    </div>
+                  )
+                }
+              }}
+            </el-tooltip>
+          </div>
+        )
       }
-    },
-    { field: 'createDate', title: '创建时间', width: 120, align: 'left' }
+    }
+    // { field: 'createDate', title: '创建时间', width: 120, align: 'left' }
   ]
 }
 
