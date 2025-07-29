c8a8cbb4115a63f78e67891f6bcb255720f02ac5
283591387@qq.com <jxx>
2025-07-29 12:10:25 +0800

    增加导入表字段主键为guid类型时设置默认主键值

--- vol.api/VOL.Core/BaseProvider/ServiceBase.cs
+++ vol.api/VOL.Core/BaseProvider/ServiceBase.cs
@@ -477,6 +477,21 @@ namespace VOL.Core.BaseProvider
             }
             if (CheckResponseResult()) return Response;
             List<T> list = Response.Data as List<T>;
+
+            var keyPro = typeof(T).GetKeyProperty();
+            if (keyPro.PropertyType == typeof(Guid) )
+            {
+                foreach (var item in list)
+                {
+                    keyPro.SetValue(item, Guid.NewGuid());
+                }
+            }
+            if (ImportOnExecuting != null)
+            {
+                Response = ImportOnExecuting.Invoke(list);
+                if (CheckResponseResult()) return Response;
+            }
+
             if (ImportOnExecuting != null)
             {
                 Response = ImportOnExecuting.Invoke(list);
